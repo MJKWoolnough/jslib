c1b6958487447814cf8eb059488f1d90f76ddb12
Michael Woolnough <michael.woolnough@gmail.com>
2018-09-24 15:46:47 +0100

    best attempt to guest correct path for relative urls

--- load.js
+++ load.js
@@ -1,20 +1,22 @@
 "use strict";
 const pageLoad = document.readyState === "complete" ? Promise.resolve() : new Promise(successFn => window.addEventListener("load", successFn)),
       {offer, include} = (function() {
-	const included = new Map(),
+	const toURL = url => (new URL(url, (document.currentScript ? document.currentScript.src : window.location.href).match(/.*\//))).href,
+	      included = new Map(),
 	      offer = obj => document.currentScript.dispatchEvent(new CustomEvent("executed", {"detail": obj})),
 	      include = (function() {
 		return function(url) {
-			if (included.has(url)) {
-				return included.get(url);
+			const aURL = toURL(url);
+			if (included.has(aURL)) {
+				return included.get(aURL);
 			}
 			const p = new Promise((successFn, errorFn) => {
 				const elm = document.createElement("script");
-				elm.setAttribute("src", url);
+				elm.setAttribute("src", aURL);
 				elm.setAttribute("type", "application/javascript")
 				elm.addEventListener("error", () => {
 					document.head.removeChild(elm);
-					errorFn(new URIError("error including: " + url));
+					errorFn(new URIError("error including: " + aURL));
 				});
 				elm.addEventListener("executed", ({detail}) => {
 					document.head.removeChild(elm);
@@ -22,7 +24,7 @@ const pageLoad = document.readyState === "complete" ? Promise.resolve() : new Pr
 				});
 				document.head.appendChild(elm);
 			});
-			included.set(url, p);
+			included.set(aURL, p);
 			return p;
 		}
 	      }());
