a271ff831916d295ec97fd7de353d664875c4c2a
Michael Woolnough <michael.woolnough@gmail.com>
2018-10-08 21:28:05 +0100

    Relative XMLHTTP URLs should also be determined by the calling script's location

--- conn.js
+++ conn.js
@@ -1,11 +1,13 @@
 offer((async function() {
 	const {Subscription} = await include("inter.js"),
+	      urlRe = /[^(@]*[(@](.+?):[0-9]+:[0-9]+[)\n]/g,
+	      toURL = url => (new URL(url, (document.currentScript ? document.currentScript.src : new Error().stack.replace(urlRe, "$1\n").split("\n")[2]).match(/.*\//))).href,
 	      HTTPRequest = function(url, props = {}) {
 		return new Promise((successFn, errorFn) => {
 			const xh = new XMLHttpRequest();
 			xh.open(
 				props.hasOwnProperty("method") ? props["method"] : "GET",
-				url,
+				toURL(url),
 				true,
 				props.hasOwnProperty("user") ? props["user"] : null,
 				props.hasOwnProperty("password") ? props["password"] : null
@@ -37,10 +39,8 @@ offer((async function() {
 			xh.send(props.hasOwnProperty("data") ? props["data"] : null);
 		});
 	      },
-	      urlRe = /[^(@]*[(@](.+?):[0-9]+:[0-9]+[)\n]/g,
-	      toURL = url => (new URL(url, (document.currentScript ? document.currentScript.src : new Error().stack.replace(urlRe, "$1\n").split("\n")[2]).match(/.*\//))).href.replace(/^http/, "ws"),
 	      WS = function(path) {
-		const url = toURL(path);
+		const url = toURL(path).replace(/^http/, "ws");
 		return new Promise((successFn, errorFn) => {
 			const ws = new WebSocket(url);
 			ws.addEventListener("open", () => successFn(Object.freeze({
