48d5d6195dcf29bc85fdc81ab89cdcf78ddbd362
Michael Woolnough <michael.woolnough@gmail.com>
2019-03-13 14:09:44 +0000

    small syntax change

--- rpc_ws.js
+++ rpc_ws.js
@@ -3,27 +3,25 @@
 import {RequestHandler} from './rpc_shared.js';
 import {WS} from './conn.js';
 
-const RPC = path => {
-	return WS(path).then(ws => {
-		const rh = new RequestHandler(ws.send),
-		      closer = () => {
+const RPC = path => WS(path).then(ws => {
+	const rh = new RequestHandler(ws.send),
+	      closer = () => {
+		if (rh.close()) {
+			ws.close();
+		}
+	      };
+	ws.when(rh.handleMessage.bind(rh), rh.handleError.bind(rh));
+	window.addEventListener("beforeunload", closer);
+	return Object.freeze({
+		"request": rh.request.bind(rh),
+		"await": rh.await.bind(rh),
+		"close": (...data) => {
 			if (rh.close()) {
-				ws.close();
+				ws.close(...data);
+				window.removeEventListener("beforeunload", closer);
 			}
-		      };
-		ws.when(rh.handleMessage.bind(rh), rh.handleError.bind(rh));
-		window.addEventListener("beforeunload", closer);
-		return Object.freeze({
-			"request": rh.request.bind(rh),
-			"await": rh.await.bind(rh),
-			"close": (...data) => {
-				if (rh.close()) {
-					ws.close(...data);
-					window.removeEventListener("beforeunload", closer);
-				}
-			}
-		});
+		}
 	});
-      };
+      });
 
 export {RPC};
