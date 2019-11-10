82ab3735fc7aaff23484d448ecd8da7fee75ba0c
Michael Woolnough <michael.woolnough@gmail.com>
2019-11-10 15:30:03 +0000

    changed how load.{js/ts} is used

--- lib.ts/load.ts
+++ lib.ts/load.ts
@@ -1,6 +1,12 @@
+/*
+
+This file should be included in an HTML script element, and it creates two globally accessible features, which can be added to a TypeScript file with the following declarations
+
+declare const pageLoad: Promise<void>;
+declare const include: (url: string) => Promise<Object>;
+*/
+
 Object.defineProperties(window, {
 	"pageLoad": {value: document.readyState === "complete" ? Promise.resolve() : new Promise(successFn => window.addEventListener("load", successFn))},
 	"include": {value: (url: string) => import(url)}
 });
-
-export {};
