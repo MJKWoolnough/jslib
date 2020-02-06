bf9600181b9a3c2529e203b765eff797ad7502b3
Michael Woolnough <michael.woolnough@gmail.com>
2020-02-06 22:46:32 +0000

    now uses a single param to specify lib directory

--- dom.sh
+++ dom.sh
@@ -4,8 +4,8 @@
 tags="a abbr address applet area article aside audio b base basefont bdi bdo blockquote body br button canvas caption cite code col colgroup data datalist dd del details dfn dialog dir div dl dt em embed fieldset figcaption figure font footer form frame frameset h1 h2 h3 h4 h5 h6 head header hgroup hr html i iframe img input ins kbd label legend li link main map mark marquee menu meta meter nav noscript object ol optgroup option output p param picture pre progress q rp rt ruby s samp script section select slot small source span strong style sub summary sup table tbody td template textarea tfoot th thead time title tr track u ul var video wbr";
 
 if [ "$1" = "lib" ]; then
-	sed '/\["'${2/\//\\\/}'"/q';
-	echo "	const {createHTML} = include(\"$3\", true);";
+	sed '/\["'${2/\//\\\/}'dom.js"/q';
+	echo "	const {createHTML} = include(\"$2html.js\", true);";
 	echo "	yield * \"$tags\".split(\" \").map(e => [e.replace(/^var$/, \"vare\"), {\"value\": createHTML.bind(null, e)}]);";
 	sed -n '/^}]/,$p';
 	exit 0;
