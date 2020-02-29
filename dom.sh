c693552e1406d34f2252038289d0ba39d0e002ce
Michael Woolnough <michael.woolnough@gmail.com>
2020-02-29 16:35:28 +0000

    small change to how the dom lib is modified on packing while dom.sh

--- dom.sh
+++ dom.sh
@@ -4,9 +4,10 @@
 tags="a abbr address applet area article aside audio b base basefont bdi bdo blockquote body br button canvas caption cite code col colgroup data datalist dd del details dfn dialog dir div dl dt em embed fieldset figcaption figure font footer form frame frameset h1 h2 h3 h4 h5 h6 head header hgroup hr html i iframe img input ins kbd label legend li link main map mark marquee menu meta meter nav noscript object ol optgroup option output p param picture pre progress q rp rt ruby s samp script section select slot small source span strong style sub summary sup table tbody td template textarea tfoot th thead time title tr track u ul var video wbr";
 
 if [ -n "$1" ]; then
-	sed '/\["'${1/\//\\\/}'dom.js"/q';
+	sed '/\["'${1/\//\\\/}'dom.js"/Q';
+	echo "}], [\"${1}dom.js\", () => {";
 	echo "	const {createHTML} = include(\"$1html.js\", true);";
-	echo "	yield * \"$tags\".split(\" \").map(e => [e.replace(/^var$/, \"vare\"), {\"value\": createHTML.bind(null, e)}]);";
+	echo "	return \"$tags\".split(\" \").map(e => [e.replace(/^var$/, \"vare\"), {\"value\": createHTML.bind(null, e)}]);";
 	sed -n '/^}]/,$p';
 	exit 0;
 fi;
