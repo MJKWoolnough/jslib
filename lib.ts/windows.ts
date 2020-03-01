5fbe2d31e1592aa016950719db5cabac445f6e35
Michael Woolnough <michael.woolnough@gmail.com>
2020-03-01 20:34:15 +0000

    type coercing

--- lib.ts/windows.ts
+++ lib.ts/windows.ts
@@ -172,7 +172,7 @@ class NoTaskbar {
 		if (!this.list.some(i => {
 			if (i.html.childNodes.length === 0) {
 				i.html.appendChild(children);
-				i.html.setAttribute("title", window.title);
+				(i.html as HTMLLIElement).setAttribute("title", window.title);
 				window.item = i.html as HTMLLIElement
 				return true;
 			}
