6af4fafeced033065d13457f9ca9e3564ab949b5
Michael Woolnough <michael.woolnough@gmail.com>
2020-12-04 09:13:58 +0000

    added some missing SVG tags

--- svg.sh
+++ svg.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #tags="$(cat ~/.node/lib64/node_modules/typescript/lib/lib.dom.d.ts | sed -n '/^interface SVGElementTagNameMap {$/,/^}$/p' | tail -n+2 | head -n-1 | cut -d'"' -f2 | tr '\n' ' ' | sed -e 's/ $//')";
-tags="a circle clipPath defs desc ellipse feBlend feColorMatrix feComponentTransfer feComposite feConvolveMatrix feDiffuseLighting feDisplacementMap feDistantLight feFlood feFuncA feFuncB feFuncG feFuncR feGaussianBlur feImage feMerge feMergeNode feMorphology feOffset fePointLight feSpecularLighting feSpotLight feTile feTurbulence filter foreignObject g image line linearGradient marker mask metadata path pattern polygon polyline radialGradient rect script stop style svg switch symbol text textPath title tspan use view";
+tags="a animate animateMotion animateTransform circle clipPath defs desc ellipse feBlend feColorMatrix feComponentTransfer feComposite feConvolveMatrix feDiffuseLighting feDisplacementMap feDistantLight feFlood feFuncA feFuncB feFuncG feFuncR feGaussianBlur feImage feMerge feMergeNode feMorphology feOffset fePointLight feSpecularLighting feSpotLight feTile feTurbulence filter foreignObject g image line linearGradient marker mask metadata mpath path pattern polygon polyline radialGradient rect set script stop style svg switch symbol text textPath title tspan use view";
 
 if [ -n "$1" ]; then
 	sed '/\["'${1/\//\\\/}'svg.js"/Q';
@@ -23,7 +23,16 @@ fi;
 		else
 			echo ",";
 		fi;
-		echo -n "$tag = createSVG.bind(null, \"$tag\") as DOMBind<SVGElementTagNameMap[\"$tag\"]>";
+		echo -n "$tag = createSVG.bind(null, \"$tag\") as DOMBind<";
+		case "$tag" in
+		"animate"|"animateMotion"|"animateTransform")
+			echo -n "SVGA${tag:1}Element";;
+		"mpath"|"set")
+			echo -n "SVGElement";;
+		*)
+			echo -n "SVGElementTagNameMap[\"$tag\"]";;
+		esac;
+		echo -n ">";
 	done | sed -e 's/^switch /switche /';
 	echo ";";
 ) > lib.ts/svg.ts;
