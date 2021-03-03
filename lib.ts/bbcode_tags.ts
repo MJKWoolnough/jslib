6f3f3fdcb4f040ad1ee6f240e6529cb2034a608f
Michael Woolnough <michael.woolnough@gmail.com>
2021-03-03 18:24:03 +0000

    added alignment tags

--- lib.ts/bbcode_tags.ts
+++ lib.ts/bbcode_tags.ts
@@ -1,21 +1,34 @@
 import type {Parsers, Tokeniser} from './bbcode.js';
+import type {DOMBind} from './dom.js';
 import {text as textSymbol, isOpenTag, process} from './bbcode.js';
-import {span} from './html.js';
+import {div, span} from './html.js';
 
-const simple = (style: string) => (n: Node, t: Tokeniser, p: Parsers) => {
+const simple = (fn: DOMBind<Node>, style: string) => (n: Node, t: Tokeniser, p: Parsers) => {
 	const tk = t.next(true).value;
 	if (tk && isOpenTag(tk)) {
-		process(n.appendChild(span({style})), t, p, tk.tagName);
+		process(n.appendChild(fn({style})), t, p, tk.tagName);
 	}
       };
 
-export const b = simple("font-weight: bold;"),
-i = simple("font-style: italic"),
-u = simple("text-decoration: underline"),
+export const b = simple(span, "font-weight: bold;"),
+i = simple(span, "font-style: italic"),
+u = simple(span, "text-decoration: underline"),
+left = simple(div, "text-align: left"),
+centre = simple(div, "text-align: center"),
+center = centre,
+right = simple(div, "text-align: right"),
+justify = simple(div, "text-align: justify"),
+full = justify,
 text = (n: Node, t: string) => n.appendChild(document.createTextNode(t)),
 all = {
 	b,
 	i,
 	u,
+	left,
+	centre,
+	center,
+	right,
+	justify,
+	full,
 	[textSymbol]: text
 };
