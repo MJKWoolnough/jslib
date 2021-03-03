04cc4bd664f4943b2103d19f4382c8765a3f90c4
Michael Woolnough <michael.woolnough@gmail.com>
2021-03-03 18:24:09 +0000

    added alignment tags

--- lib.js/bbcode_tags.js
+++ lib.js/bbcode_tags.js
@@ -1,20 +1,32 @@
 import {text as textSymbol, isOpenTag, process} from './bbcode.js';
-import {span} from './html.js';
+import {div, span} from './html.js';
 
-const simple = style => (n, t, p) => {
+const simple = (fn, style) => (n, t, p) => {
 	const tk = t.next(true).value;
 	if (tk && isOpenTag(tk)) {
-		process(n.appendChild(span({style})), t, p, tk.tagName);
+		process(n.appendChild(fn({style})), t, p, tk.tagName);
 	}
       };
 
-export const b = simple("font-weight: bold;"),
-i = simple("font-style: italic"),
-u = simple("text-decoration: underline"),
+export const b = simple(span, "font-weight: bold;"),
+i = simple(span, "font-style: italic"),
+u = simple(span, "text-decoration: underline"),
+left = simple(div, "text-align: left"),
+centre = simple(div, "text-align: center"),
+center = centre,
+right = simple(div, "text-align: right"),
+justify = simple(div, "text-align: justify"),
+full = justify,
 text = (n, t) => n.appendChild(document.createTextNode(t)),
 all = {
 	b,
 	i,
 	u,
+	left,
+	centre,
+	center,
+	right,
+	justify,
+	full,
 	[textSymbol]: text
 };
