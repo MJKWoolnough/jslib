90ada614291919c3b04166985e5362833812b89d
Michael Woolnough <michael.woolnough@gmail.com>
2021-06-17 11:30:09 +0100

    start of structure for module parsing

--- jslib.go
+++ jslib.go
@@ -8,9 +8,15 @@ import (
 	"vimagination.zapto.org/parser"
 )
 
-type config struct {
-	filesToDo []string
+type data struct {
+	url    string
+	module *javascript.Module
+}
 
+type config struct {
+	filesToDo    []string
+	filesDone    map[string]struct{}
+	files        []data
 	loader       func(string) (*javascript.Module, error)
 	bare         bool
 	parseDynamic bool
@@ -32,6 +38,23 @@ func Package(os ...Option) (*javascript.Module, error) {
 	}
 	for _, o := range os {
 		o(&c)
+	}
+	for len(c.filesToDo) > 0 {
+		url := c.filesToDo[0]
+		c.filesToDo = c.filesToDo[1:]
+		m, err := c.loader(url)
+		if err != nil {
+			return nil, err
+		}
+		for _, li := range m.ModuleListItems {
+			if li.ImportDeclaration != nil {
+
+			} else if c.parseDynamic && li.StatementListItem != nil {
+
+			} else if !c.bare && li.ExportDeclaration != nil {
+
+			}
+		}
 	}
 	return nil, nil
 }
