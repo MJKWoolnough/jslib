35ebe70d8ef5906fd56830d418d5988ecd87c092
Michael Woolnough <michael.woolnough@gmail.com>
2021-06-17 10:28:33 +0100

    basic structure

---
+++ jslib.go
@@ -0,0 +1,36 @@
+package jslib
+
+import (
+	"os"
+	"path/filepath"
+
+	"vimagination.zapto.org/javascript"
+	"vimagination.zapto.org/parser"
+)
+
+type config struct {
+	filesToDo []string
+
+	loader func(string) (*javascript.Module, error)
+	bare   bool
+}
+
+func OSLoad(url string) (*javascript.Module, error) {
+	f, err := os.Open(filepath.FromSlash(url))
+	if err != nil {
+		return nil, err
+	}
+	m, err := javascript.ParseModule(parser.NewReaderTokeniser(f))
+	f.Close()
+	return m, err
+}
+
+func Package(os ...Option) (*javascript.Module, error) {
+	c := config{
+		loader: OSLoad,
+	}
+	for _, o := range os {
+		o(&c)
+	}
+	return nil, nil
+}
