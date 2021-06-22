e5369d7252312602040b83054a600dbb2d085a4b
Michael Woolnough <michael.woolnough@gmail.com>
2021-06-22 12:58:23 +0100

    used new javascript/walk package to simply replace import calls with include calls (may need to check name for collisions)

--- jslib.go
+++ jslib.go
@@ -5,6 +5,7 @@ import (
 	"path/filepath"
 
 	"vimagination.zapto.org/javascript"
+	"vimagination.zapto.org/javascript/walk"
 	"vimagination.zapto.org/parser"
 )
 
@@ -20,6 +21,7 @@ type config struct {
 	loader       func(string) (*javascript.Module, error)
 	bare         bool
 	parseDynamic bool
+	currURL      string
 }
 
 func OSLoad(url string) (*javascript.Module, error) {
@@ -40,21 +42,41 @@ func Package(os ...Option) (*javascript.Module, error) {
 		o(&c)
 	}
 	for len(c.filesToDo) > 0 {
-		url := c.filesToDo[0]
+		c.currURL = c.filesToDo[0]
 		c.filesToDo = c.filesToDo[1:]
-		m, err := c.loader(url)
+		m, err := c.loader(c.currURL)
 		if err != nil {
 			return nil, err
 		}
 		for _, li := range m.ModuleListItems {
 			if li.ImportDeclaration != nil {
 
-			} else if c.parseDynamic && li.StatementListItem != nil {
-
+			} else if li.StatementListItem != nil {
+				if err := walk.Walk(li.StatementListItem, &c); err != nil {
+					return nil, err
+				}
 			} else if !c.bare && li.ExportDeclaration != nil {
 
 			}
 		}
 	}
 	return nil, nil
+}
+
+func (c *config) Handle(t javascript.Type) error {
+	if ce, ok := t.(*javascript.CallExpression); ok && ce.ImportCall != nil {
+		ce.MemberExpression = &javascript.MemberExpression{
+			PrimaryExpression: &javascript.PrimaryExpression{
+				IdentifierReference: &javascript.Token{Token: parser.Token{Data: "include"}},
+			},
+			Arguments: &javascript.Arguments{
+				ArgumentList: []javascript.AssignmentExpression{
+					*ce.ImportCall,
+				},
+			},
+		}
+		ce.ImportCall = nil
+		return nil
+	}
+	return walk.Walk(t, c)
 }
