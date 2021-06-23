ca95af1d538841f3c471e27bb8572536752c88f5
Michael Woolnough <michael.woolnough@gmail.com>
2021-06-23 10:09:05 +0100

    changed how urls are initially stored + made sure each url is only processed once

--- jslib.go
+++ jslib.go
@@ -5,17 +5,19 @@ import (
 	"path/filepath"
 
 	"vimagination.zapto.org/javascript"
+	"vimagination.zapto.org/javascript/scope"
 	"vimagination.zapto.org/javascript/walk"
 	"vimagination.zapto.org/parser"
 )
 
 type data struct {
 	url    string
+	scope  *scope.Scope
 	module *javascript.Module
 }
 
 type config struct {
-	filesToDo    []string
+	filesToDo    []data
 	filesDone    map[string]struct{}
 	files        []data
 	loader       func(string) (*javascript.Module, error)
@@ -41,29 +43,39 @@ func Package(os ...Option) (*javascript.Module, error) {
 	for _, o := range os {
 		o(&c)
 	}
+	var err error
 	for len(c.filesToDo) > 0 {
-		c.currURL = c.filesToDo[0]
+		d := c.filesToDo[0]
+		if _, ok := c.filesDone[d.url]; ok {
+			continue
+		}
+		c.filesDone[d.url] = struct{}{}
 		c.filesToDo = c.filesToDo[1:]
-		m, err := c.loader(c.currURL)
+		d.module, err = c.loader(d.url)
 		if err != nil {
 			return nil, err
 		}
-		for _, li := range m.ModuleListItems {
+		d.scope, err = scope.ModuleScope(d.module, nil)
+		if err != nil {
+			return nil, err
+		}
+		for _, li := range d.module.ModuleListItems {
 			if li.ImportDeclaration != nil {
 
 			} else if li.StatementListItem != nil {
-				if err := walk.Walk(li.StatementListItem, &c); err != nil {
+				if err := walk.Walk(li.StatementListItem, &d); err != nil {
 					return nil, err
 				}
 			} else if !c.bare && li.ExportDeclaration != nil {
 
 			}
 		}
+		c.files = append(c.files, d)
 	}
 	return nil, nil
 }
 
-func (c *config) Handle(t javascript.Type) error {
+func (d *data) Handle(t javascript.Type) error {
 	if ce, ok := t.(*javascript.CallExpression); ok && ce.ImportCall != nil {
 		ce.MemberExpression = &javascript.MemberExpression{
 			PrimaryExpression: &javascript.PrimaryExpression{
@@ -78,5 +90,5 @@ func (c *config) Handle(t javascript.Type) error {
 		ce.ImportCall = nil
 		return nil
 	}
-	return walk.Walk(t, c)
+	return walk.Walk(t, d)
 }
