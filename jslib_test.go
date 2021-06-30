7d129843c0b51157debd4608716877e2aa31c0ac
Michael Woolnough <michael.woolnough@gmail.com>
2021-06-30 12:12:40 +0100

    added multi-rename export test

--- jslib_test.go
+++ jslib_test.go
@@ -62,6 +62,15 @@ func TestPackage(t *testing.T) {
 			"Object.defineProperties(window, {pageLoad: {value: document.readyState == \"complete\" ? Promise.resolve() : new Promise(successFn => window.addEventListener(\"load\", successFn, {once: true}))}, include: {value: (() => {\n\tconst imports = new Map([[\"a.js\"], [\"b.js\", [\"c\", () => b_c]]].map(([url, ...props]) => [url, Object.freeze(Object.defineProperties({}, Object.fromEntries(props.map(([prop, get, set]) => [prop, {enumerable: true, get, set}]))))]));\n\treturn url => Promise.resolve(imports.get(url) ?? import(url));\n})()}});\n\nconst b_c = 1;\n\nconsole.log(b_c);",
 			[]Option{File("a.js")},
 		},
+		{
+			loader{
+				path.Join(cwd, "a.js"): "import {f as g} from './b.js'; console.log(g)",
+				path.Join(cwd, "b.js"): "export {e as f} from './c.js'",
+				path.Join(cwd, "c.js"): "const d = 1;export {d as e}",
+			},
+			"Object.defineProperties(window, {pageLoad: {value: document.readyState == \"complete\" ? Promise.resolve() : new Promise(successFn => window.addEventListener(\"load\", successFn, {once: true}))}, include: {value: (() => {\n\tconst imports = new Map([[\"a.js\"], [\"b.js\", [\"f\", () => c_d]], [\"c.js\", [\"e\", () => c_d]]].map(([url, ...props]) => [url, Object.freeze(Object.defineProperties({}, Object.fromEntries(props.map(([prop, get, set]) => [prop, {enumerable: true, get, set}]))))]));\n\treturn url => Promise.resolve(imports.get(url) ?? import(url));\n})()}});\n\nconst c_d = 1;\n\nconsole.log(c_d);",
+			[]Option{File("a.js")},
+		},
 	} {
 		s, err := Package(append(test.Options, Loader(test.Input.load))...)
 		if err != nil {
