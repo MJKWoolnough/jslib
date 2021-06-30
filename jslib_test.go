e5c493414d92dcae8b22934c8707caa65dae9a2d
Michael Woolnough <michael.woolnough@gmail.com>
2021-06-30 10:36:52 +0100

    wrote proper testing framework

--- jslib_test.go
+++ jslib_test.go
@@ -20,35 +20,39 @@ func (l loader) load(url string) (*javascript.Module, error) {
 	return javascript.ParseModule(parser.NewStringTokeniser(d))
 }
 
-func runTest(t *testing.T, expectedOutput string, opts ...Option) {
-	t.Helper()
-	s, err := Package(opts...)
-	if err != nil {
-		t.Fatalf("unexpected err: %s", err)
-	}
-	output := fmt.Sprintf("%s", s)
-	if output != expectedOutput {
-		t.Errorf("expecting output: %q\ngot: %q", expectedOutput, output)
-	}
-}
-
-func Test1(t *testing.T) {
+func TestPackage(t *testing.T) {
 	cwd, _ := os.Getwd()
-	l := loader{path.Join(cwd, "a.js"): "1"}
-	runTest(t, "Object.defineProperties(window, {pageLoad: {value: document.readyState == \"complete\" ? Promise.resolve() : new Promise(successFn => window.addEventListener(\"load\", successFn, {once: true}))}, include: {value: (() => {\n\tconst imports = new Map([[\"a.js\"]].map(([url, ...props]) => [url, Object.freeze(Object.defineProperties({}, Object.fromEntries(props.map(([prop, get, set]) => [prop, {enumerable: true, get, set}]))))]));\n\treturn url => Promise.resolve(imports.get(url) ?? import(url));\n})()}});\n\n1;", Loader(l.load), File("a.js"))
-}
-
-func Test2(t *testing.T) {
-	cwd, _ := os.Getwd()
-	l := loader{path.Join(cwd, "a.js"): "1"}
-	runTest(t, "Object.defineProperties(window, {pageLoad: {value: document.readyState == \"complete\" ? Promise.resolve() : new Promise(successFn => window.addEventListener(\"load\", successFn, {once: true}))}, include: {value: url => import(url)}});\n\n1;", Loader(l.load), File("a.js"), NoExports)
-}
-
-func Test3(t *testing.T) {
-	cwd, _ := os.Getwd()
-	l := loader{
-		path.Join(cwd, "a.js"): "import {c} from './b.js'; console.log(c)",
-		path.Join(cwd, "b.js"): "export const c = 1",
+	for n, test := range [...]struct {
+		Input   loader
+		Output  string
+		Options []Option
+	}{
+		{
+			loader{path.Join(cwd, "a.js"): "1"},
+			"Object.defineProperties(window, {pageLoad: {value: document.readyState == \"complete\" ? Promise.resolve() : new Promise(successFn => window.addEventListener(\"load\", successFn, {once: true}))}, include: {value: (() => {\n\tconst imports = new Map([[\"a.js\"]].map(([url, ...props]) => [url, Object.freeze(Object.defineProperties({}, Object.fromEntries(props.map(([prop, get, set]) => [prop, {enumerable: true, get, set}]))))]));\n\treturn url => Promise.resolve(imports.get(url) ?? import(url));\n})()}});\n\n1;",
+			[]Option{File("a.js")},
+		},
+		{
+			loader{path.Join(cwd, "a.js"): "1"},
+			"Object.defineProperties(window, {pageLoad: {value: document.readyState == \"complete\" ? Promise.resolve() : new Promise(successFn => window.addEventListener(\"load\", successFn, {once: true}))}, include: {value: url => import(url)}});\n\n1;",
+			[]Option{File("a.js"), NoExports},
+		},
+		{
+			loader{
+				path.Join(cwd, "a.js"): "import {c} from './b.js'; console.log(c)",
+				path.Join(cwd, "b.js"): "export const c = 1",
+			},
+			"Object.defineProperties(window, {pageLoad: {value: document.readyState == \"complete\" ? Promise.resolve() : new Promise(successFn => window.addEventListener(\"load\", successFn, {once: true}))}, include: {value: (() => {\n\tconst imports = new Map([[\"a.js\"], [\"b.js\", [\"c\", () => b_c]]].map(([url, ...props]) => [url, Object.freeze(Object.defineProperties({}, Object.fromEntries(props.map(([prop, get, set]) => [prop, {enumerable: true, get, set}]))))]));\n\treturn url => Promise.resolve(imports.get(url) ?? import(url));\n})()}});\n\nconst b_c = 1;\n\nconsole.log(b_c);",
+			[]Option{File("a.js")},
+		},
+	} {
+		s, err := Package(append(test.Options, Loader(test.Input.load))...)
+		if err != nil {
+			t.Fatalf("test %d: unexpected err: %s", n+1, err)
+		}
+		output := fmt.Sprintf("%s", s)
+		if output != test.Output {
+			t.Errorf("test %d: expecting output: %q\ngot: %q", n+1, test.Output, output)
+		}
 	}
-	runTest(t, "Object.defineProperties(window, {pageLoad: {value: document.readyState == \"complete\" ? Promise.resolve() : new Promise(successFn => window.addEventListener(\"load\", successFn, {once: true}))}, include: {value: (() => {\n\tconst imports = new Map([[\"a.js\"], [\"b.js\", [\"c\", () => b_c]]].map(([url, ...props]) => [url, Object.freeze(Object.defineProperties({}, Object.fromEntries(props.map(([prop, get, set]) => [prop, {enumerable: true, get, set}]))))]));\n\treturn url => Promise.resolve(imports.get(url) ?? import(url));\n})()}});\n\nconst b_c = 1;\n\nconsole.log(b_c);", Loader(l.load), File("a.js"))
 }
