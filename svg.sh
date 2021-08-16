ee340371d6cfb77f1979eb96f099bf57f8f2ed4f
Michael Woolnough <michael.woolnough@gmail.com>
2021-08-16 07:44:05 +0100

    added ability to create custom svg files for projects

--- svg.sh
+++ svg.sh
@@ -3,71 +3,83 @@
 #tags="$(cat ~/.node/lib64/node_modules/typescript/lib/lib.dom.d.ts | sed -n '/^interface SVGElementTagNameMap {$/,/^}$/p' | tail -n+2 | head -n-1 | cut -d'"' -f2 | tr '\n' ' ' | sed -e 's/ $//')";
 tags="a animate animateMotion animateTransform circle clipPath defs desc ellipse feBlend feColorMatrix feComponentTransfer feComposite feConvolveMatrix feDiffuseLighting feDisplacementMap feDistantLight feFlood feFuncA feFuncB feFuncG feFuncR feGaussianBlur feImage feMerge feMergeNode feMorphology feOffset fePointLight feSpecularLighting feSpotLight feTile feTurbulence filter foreignObject g image line linearGradient marker mask metadata mpath path pattern polygon polyline radialGradient rect set script stop style svg switch symbol text textPath title tspan use view";
 
+ts="lib.ts/svg.ts";
+js="lib.js/svg.js";
+
 if [ -n "$1" ]; then
-	sed '/\["'${1/\//\\\/}'svg.js"/Q';
-	echo "}], [\"${1}svg.js\", () => {";
-	echo "	const {createSVG} = include(\"$1dom.js\", true);";
-	echo "	return \"$tags\".split(\" \").map(e => [e.replace(/^switch$/, \"switche\"), {\"value\": createSVG.bind(null, e)}]).concat([[\"createSVG\", {\"value\": createSVG}]]);";
-	sed -n '/^}]/,$p';
-	exit 0;
+	tags="$1";
+	if [ -n "$2" ]; then
+		case "$2" in
+		*".js")
+			js="$2";
+			ts="";;
+		*".ts")
+			js="";
+			ts="$2";;
+		esac;
+	fi;
 fi;
 
-(
-	echo -e "import type {DOMBind} from './dom.js';";
-	echo -e "import {createSVG} from './dom.js';\n";
-	echo -e "export {createSVG};\n";
-	echo -n "export const [";
-	first=true;
-	for tag in $tags; do
-		if $first; then
-			first=false;
-		else
-			echo -n ", ";
-		fi;
-		if [ "$tag" = "switch" ]; then
-			echo -n "switche";
-		else
-			echo -n "$tag";
-		fi;
-	done;
-	echo -n "] = \"$tags\".split(\" \").map(e => createSVG.bind(null, e)) as [";
-	first=true;
-	for tag in $tags; do
-		if $first; then
-			first=false;
-		else
-			echo -n ",";
-		fi;
-		echo -n "DOMBind<";
-		case "$tag" in
-		"animate"|"animateMotion"|"animateTransform")
-			echo -n "SVGA${tag:1}Element";;
-		"mpath"|"set")
-			echo -n "SVGElement";;
-		*)
-			echo -n "SVGElementTagNameMap[\"$tag\"]";;
-		esac;
-		echo -n ">";
-	done;
-	echo "];";
-) > lib.ts/svg.ts;
+if [ -n "$ts" ]; then
+	(
+		echo -e "import type {DOMBind} from './dom.js';";
+		echo -e "import {createSVG} from './dom.js';\n";
+		echo -e "export {createSVG};\n";
+		echo -n "export const [";
+		first=true;
+		for tag in $tags; do
+			if $first; then
+				first=false;
+			else
+				echo -n ", ";
+			fi;
+			if [ "$tag" = "switch" ]; then
+				echo -n "switche";
+			else
+				echo -n "$tag";
+			fi;
+		done;
+		echo -n "] = \"$tags\".split(\" \").map(e => createSVG.bind(null, e)) as [";
+		first=true;
+		for tag in $tags; do
+			if $first; then
+				first=false;
+			else
+				echo -n ",";
+			fi;
+			echo -n "DOMBind<";
+			case "$tag" in
+			"animate"|"animateMotion"|"animateTransform")
+				echo -n "SVGA${tag:1}Element";;
+			"mpath"|"set")
+				echo -n "SVGElement";;
+			*)
+				echo -n "SVGElementTagNameMap[\"$tag\"]";;
+			esac;
+			echo -n ">";
+		done;
+		echo "];";
+	) > "$ts";
+fi;
 
-(
-	echo -e "import {createSVG} from './dom.js';\n";
-	echo -e "export {createSVG};\n";
-	echo -n "export const [";
-	first=true;
-	for tag in $tags; do
-		if $first; then
-			first=false;
-		else
-			echo -n ", ";
-		fi;
-		if [ "$tag" = "switch" ]; then
-			echo -n "switche";
-		else
-			echo -n "$tag";
-		fi;
-	done;
-	echo "] = \"$tags\".split(\" \").map(e => createSVG.bind(null, e));";
-) > lib.js/svg.js;
+if [ -n "$js" ]; then
+	(
+		echo -e "import {createSVG} from './dom.js';\n";
+		echo -e "export {createSVG};\n";
+		echo -n "export const [";
+		first=true;
+		for tag in $tags; do
+			if $first; then
+				first=false;
+			else
+				echo -n ", ";
+			fi;
+			if [ "$tag" = "switch" ]; then
+				echo -n "switche";
+			else
+				echo -n "$tag";
+			fi;
+		done;
+		echo "] = \"$tags\".split(\" \").map(e => createSVG.bind(null, e));";
+	) > "$js";
+fi;
