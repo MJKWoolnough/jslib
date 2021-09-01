13b9f19ba51d4cfae762d4a647d8179a4c9c0b9f
Michael Woolnough <michael.woolnough@gmail.com>
2021-09-01 14:30:48 +0100

    added script that will scan a dependency tree and create a list of all svg.js (non-createSVG) imports used. Useful when combined with svg.sh to create a package-custom svg.js/ts lib

---
+++ requiredSVG.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+filesDone="";
+tags="";
+
+function scanFile {
+	declare file="$1";
+	if [ ! -z "$(echo "$filesDone" | grep "$file")" ]; then
+		return;
+	fi;
+	filesDone="$(echo -e "$filesDone\n$file")";
+	declare dir="$(dirname "$file")";
+	declare import;
+	while read import; do
+		declare realImport="$(realpath "$dir/$(echo "$import" | cut -d"'" -f2)")";
+		scanFile "$realImport";
+		if [ ! -z "$(echo "$realImport" | sed -ne '/lib[^\/]*\/svg.js$/p')" ]; then
+			if [ ! -z "$(echo "$import" | grep "*")" ]; then
+				echo "!!";
+				exit 1;
+			fi;
+			tags="$tags,$(echo "$import" | cut -d'{' -f2 | cut -d'}' -f1 | sed -e 's/ as [^,}]*//g' | tr -d ' ')";
+		fi;
+	done < <(grep "^import " "$file" | grep -v "^import type");
+}
+
+for f; do
+	scanFile "$(realpath "$f")";
+done;
+
+echo -n "\"";
+echo "$tags" | tr ',' '\n' | grep -v "^$" | grep -v "createSVG" | sort | uniq | tr '\n' ' ' | sed -e 's/ $//';
+echo "\"";
