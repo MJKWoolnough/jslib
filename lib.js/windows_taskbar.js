b363affeeebea875cb838a0df850de7b5a977643
Michael Woolnough <michael.woolnough@gmail.com>
2022-01-01 16:34:07 +0000

    corrected indentation

--- lib.js/windows_taskbar.js
+++ lib.js/windows_taskbar.js
@@ -77,7 +77,7 @@ export class ShellElement extends BaseShellElement {
 					}
 				});
 			}}))
-		      ]);
+		]);
 	}
 }
 
--- lib.ts/windows_taskbar.ts
+++ lib.ts/windows_taskbar.ts
@@ -206,7 +206,7 @@ export class ShellElement extends BaseShellElement {
 					}
 				});
 			}}))
-		      ]);
+		]);
 	}
 }
 
