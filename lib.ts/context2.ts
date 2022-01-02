12f9375ef94a6875636b1a940906df6d281f8e28
Michael Woolnough <michael.woolnough@gmail.com>
2022-01-02 18:09:11 +0000

    set up initial slots

--- lib.ts/context2.ts
+++ lib.ts/context2.ts
@@ -1,13 +1,29 @@
 import type {Children, Props} from './dom.js';
 import {makeElement} from './dom.js';
+import {div, slot} from './html.js';
 
 export class ContextMenu extends HTMLElement {
+	constructor() {
+		super();
+		this.setAttribute("slot", "context-menu");
+		makeElement(this.attachShadow({"mode": "closed"}), [
+			div(slot({"name": "context-item"}))
+		]);
+	}
 }
 
 export class ContextItem extends HTMLElement {
+	constructor() {
+		super();
+		this.setAttribute("slot", "context-item");
+	}
 }
 
 export class ContextTitle extends HTMLElement {
+	constructor() {
+		super();
+		this.setAttribute("slot", "context-title");
+	}
 }
 
 customElements.define("context-menu", ContextMenu);
