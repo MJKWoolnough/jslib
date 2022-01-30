363a81a10a54caac9e4c0786acabba864064369a
Michael Woolnough <michael.woolnough@gmail.com>
2022-01-30 11:19:06 +0000

    removed old test fn

--- test/test.ts
+++ test/test.ts
@@ -1,34 +1,5 @@
 declare const pageLoad: Promise<void>;
 
-const test = (() => {
-	const handleError = (e: any) => {
-		console.log(e);
-		alert(e instanceof Error ? e.message : e instanceof Error ? e.message : e);
-	      },
-	      ul = document.createElement("ul"),
-	      completeSpan = document.createElement("span"),
-	      totalSpan = document.createElement("span");
-	let p = pageLoad.then(() => {
-		document.body.innerText = "";
-		document.body.append("Tests: ", completeSpan, "/", totalSpan, ul);
-	    }),
-	    complete = 0,
-	    total = 0;
-	return (desc: string, fn: () => boolean | Promise<boolean>) => {
-		const li = document.createElement("li");
-		li.innerText = desc;
-		p = p.finally(async () => {
-			const pass = await fn();
-			li.setAttribute("class", pass ? "pass" : "fail");
-			if (pass) {
-				completeSpan.innerText = ++complete + "";
-			}
-		}).catch(handleError);
-		ul.append(li);
-		totalSpan.innerText = ++total + "";
-	};
-      })();
-
 ((data: Record<string, Record<string, Record<string, () => Promise<boolean>>>>) => {
 	const handleError = (e: any) => {
 		console.log(e);
