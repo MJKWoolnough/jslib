9b08fb1669f0fa4bcc2112354de49f26c8694780
Michael Woolnough <michael.woolnough@gmail.com>
2022-02-02 10:46:10 +0000

    embedded fns

--- test/main.go
+++ test/main.go
@@ -20,33 +20,29 @@ func run() error {
 	m := http.NewServeMux()
 	m.Handle("/", http.FileServer(http.Dir("./")))
 	m.Handle("/static", http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) { w.Write([]byte("123")) }))
-	m.Handle("/echo", http.HandlerFunc(echo))
-	m.Handle("/request", http.HandlerFunc(request))
+	m.Handle("/echo", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		ct := r.Header.Get("Content-Type")
+		if ct != "" {
+			w.Header().Add("Content-Type", ct)
+		}
+		io.Copy(w, r.Body)
+		r.Body.Close()
+	}))
+	m.Handle("/request", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		r.ParseForm()
+		json.NewEncoder(w).Encode(struct {
+			Method        string      `json:"method"`
+			Headers       http.Header `json:"headers"`
+			ContentLength int64       `json:"contentLength"`
+			Form          url.Values  `json:"form"`
+			PostForm      url.Values  `json:"postForm"`
+		}{
+			Method:        r.Method,
+			Headers:       r.Header,
+			ContentLength: r.ContentLength,
+			Form:          r.Form,
+			PostForm:      r.PostForm,
+		})
+	}))
 	return http.ListenAndServe(":8080", m)
-}
-
-func echo(w http.ResponseWriter, r *http.Request) {
-	ct := r.Header.Get("Content-Type")
-	if ct != "" {
-		w.Header().Add("Content-Type", ct)
-	}
-	io.Copy(w, r.Body)
-	r.Body.Close()
-}
-
-func request(w http.ResponseWriter, r *http.Request) {
-	r.ParseForm()
-	json.NewEncoder(w).Encode(struct {
-		Method        string      `json:"method"`
-		Headers       http.Header `json:"headers"`
-		ContentLength int64       `json:"contentLength"`
-		Form          url.Values  `json:"form"`
-		PostForm      url.Values  `json:"postForm"`
-	}{
-		Method:        r.Method,
-		Headers:       r.Header,
-		ContentLength: r.ContentLength,
-		Form:          r.Form,
-		PostForm:      r.PostForm,
-	})
 }
