cc413a3cad3319d9527edd2b8ce4871e1bd7926c
Michael Woolnough <michael.woolnough@gmail.com>
2022-03-17 10:41:35 +0000

    added lib to aid with drag+drop operations

---
+++ lib.js/drag.js
@@ -0,0 +1,66 @@
+export class DragTransfer {
+	#data = new Map();
+	#nextID = 0;
+	#format;
+	constructor(format) {
+		this.#format = format;
+	}
+	register(t) {
+		const key = this.#nextID++ + "";
+		this.#data.set(key, t);
+		return key;
+	}
+	get(e) {
+		return this.#data.get(e.dataTransfer?.getData(this.#format) ?? "")?.transfer();
+	}
+	set(e, key, icon, xOffset = -5, yOffset = -5) {
+		e.dataTransfer?.setData(this.#format, key);
+		if (icon) {
+			e.dataTransfer?.setDragImage(icon, xOffset, yOffset);
+		}
+	}
+	deregister(key) {
+		this.#data.delete(key);
+	}
+	is(e) {
+		return e.dataTransfer?.types.includes(this.#format) ?? false;
+	}
+}
+
+export class DragFiles {
+	constructor(...mimes) {
+		this.mimes = Object.freeze(mimes);
+	}
+	asForm(e, name) {
+		const f = new FormData();
+		if (e.dataTransfer) {
+			for (const file of e.dataTransfer.files) {
+				f.append(name, file);
+			}
+		}
+		return f;
+	}
+	is (e) {
+		if (e.dataTransfer?.types.includes("Files")) {
+			for (const i of e.dataTransfer.items) {
+				if (i["kind"] !== "file" || !this.mimes.includes(i["type"])) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+}
+
+export const setDragEffect = effects => e => {
+	for (const effect in effects) {
+		for (const key of effects[effect] ?? []) {
+			if (key.is(e)) {
+				e.preventDefault();
+				e.dataTransfer.dropEffect = effect;
+				return true;
+			}
+		}
+	}
+	return false;
+}
---
+++ lib.ts/drag.ts
@@ -0,0 +1,85 @@
+interface Transfer<T> {
+	transfer(): T;
+}
+
+interface CheckedDragEvent extends DragEvent {
+	dataTransfer: DataTransfer;
+}
+
+interface CheckedDT<T> extends DragTransfer<T> {
+	get(e: DragEvent): T;
+}
+
+interface Is {
+	is(e: DragEvent): boolean;
+}
+
+type EffectRecord = Partial<Record<"none" | "copy" | "link" | "move", Is[]>>;
+
+export class DragTransfer<T = any> {
+	#data = new Map<string, Transfer<T>>();
+	#nextID = 0;
+	#format: string;
+	constructor(format: string) {
+		this.#format = format;
+	}
+	register(t: Transfer<T>) {
+		const key = this.#nextID++ + "";
+		this.#data.set(key, t);
+		return key;
+	}
+	get(e: DragEvent): T | undefined {
+		return this.#data.get(e.dataTransfer?.getData(this.#format) ?? "")?.transfer();
+	}
+	set(e: DragEvent, key: string, icon?: HTMLDivElement, xOffset = -5, yOffset = -5) {
+		e.dataTransfer?.setData(this.#format, key);
+		if (icon) {
+			e.dataTransfer?.setDragImage(icon, xOffset, yOffset);
+		}
+	}
+	deregister(key: string) {
+		this.#data.delete(key);
+	}
+	is(e: DragEvent): this is CheckedDT<T> {
+		return e.dataTransfer?.types.includes(this.#format) ?? false;
+	}
+}
+
+export class DragFiles {
+	mimes: Readonly<string[]>;
+	constructor(...mimes: string[]) {
+		this.mimes = Object.freeze(mimes);
+	}
+	asForm(e: DragEvent, name: string) {
+		const f = new FormData();
+		if (e.dataTransfer) {
+			for (const file of e.dataTransfer.files) {
+				f.append(name, file);
+			}
+		}
+		return f;
+	}
+	is (e: DragEvent): e is CheckedDragEvent {
+		if (e.dataTransfer?.types.includes("Files")) {
+			for (const i of e.dataTransfer.items) {
+				if (i["kind"] !== "file" || !this.mimes.includes(i["type"])) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+}
+
+export const setDragEffect = (effects: EffectRecord) => (e: DragEvent) => {
+	for (const effect in effects) {
+		for (const key of effects[effect as keyof EffectRecord] ?? []) {
+			if (key.is(e)) {
+				e.preventDefault();
+				e.dataTransfer!.dropEffect = effect as keyof EffectRecord;
+				return true;
+			}
+		}
+	}
+	return false;
+};
