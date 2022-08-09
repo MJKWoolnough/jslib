0de5b97e6e7719214c6ad8dc5db59977f02d7f1e
Michael Woolnough <michael.woolnough@gmail.com>
2022-08-09 14:42:18 +0100

    added class to deal with fraction based math

---
+++ lib.js/fraction.js
@@ -0,0 +1,59 @@
+export default class Fraction {
+	#numerator;
+	#denominator;
+	static zero = new Fraction(0n);
+	static one = new Fraction(1n);
+	static NaN = new Fraction(0n, 0n);
+	constructor(numerator, denominator = 1n) {
+		if (denominator < 0n) {
+			this.#numerator = -numerator;
+			this.#denominator = -denominator;
+		} else {
+			this.#numerator = numerator;
+			this.#denominator = denominator;
+		}
+	}
+	add(num) {
+		if (this.#denominator === num.#denominator) {
+			return new Fraction(this.#numerator + num.#numerator, this.#denominator);
+		}
+		return new Fraction(this.#numerator * num.#denominator + this.#denominator * num.#numerator, this.#denominator * num.#denominator);
+	}
+	sub(num) {
+		if (this.#denominator === num.#denominator) {
+			return new Fraction(this.#numerator - num.#numerator, this.#denominator);
+		}
+		return new Fraction(this.#numerator * num.#denominator - this.#denominator * num.#numerator, this.#denominator * num.#denominator);
+	}
+	mul(num) {
+		return new Fraction(this.#numerator * num.#numerator, this.#denominator * num.#denominator);
+	}
+	div(num) {
+		return new Fraction(this.#numerator * num.#denominator, this.#denominator * num.#numerator);
+	}
+	cmp(num) {
+		if (!this.#denominator || !num.#denominator) {
+			return NaN;
+		}
+		const d = this.#numerator * num.#denominator - this.#denominator * num.#numerator;
+		return d < 0n ? -1 : d > 0n ? 1: 0;
+	}
+	isNaN() {
+		return !this.#denominator;
+	}
+	sign() {
+		return !this.#denominator ? NaN : !this.#numerator ? 0 : this.#numerator < 0n ? -1 : 1;
+	}
+	toFloat() {
+		if (!this.#denominator) {
+			return NaN;
+		}
+		return Number(10000n * this.#numerator / this.#denominator) / 10000;
+	}
+	static min(a, b) {
+		return a.cmp(b) === -1 ? a : b;
+	}
+	static max(a, b) {
+		return a.cmp(b) === 1 ? a : b;
+	}
+}
---
+++ lib.ts/fraction.ts
@@ -0,0 +1,59 @@
+export default class Fraction {
+	#numerator: bigint;
+	#denominator: bigint;
+	static readonly zero = new Fraction(0n);
+	static readonly one = new Fraction(1n);
+	static readonly NaN = new Fraction(0n, 0n);
+	constructor(numerator: bigint, denominator: bigint = 1n) {
+		if (denominator < 0n) {
+			this.#numerator = -numerator;
+			this.#denominator = -denominator;
+		} else {
+			this.#numerator = numerator;
+			this.#denominator = denominator;
+		}
+	}
+	add(num: Fraction) {
+		if (this.#denominator === num.#denominator) {
+			return new Fraction(this.#numerator + num.#numerator, this.#denominator);
+		}
+		return new Fraction(this.#numerator * num.#denominator + this.#denominator * num.#numerator, this.#denominator * num.#denominator);
+	}
+	sub(num: Fraction) {
+		if (this.#denominator === num.#denominator) {
+			return new Fraction(this.#numerator - num.#numerator, this.#denominator);
+		}
+		return new Fraction(this.#numerator * num.#denominator - this.#denominator * num.#numerator, this.#denominator * num.#denominator);
+	}
+	mul(num: Fraction) {
+		return new Fraction(this.#numerator * num.#numerator, this.#denominator * num.#denominator);
+	}
+	div(num: Fraction) {
+		return new Fraction(this.#numerator * num.#denominator, this.#denominator * num.#numerator);
+	}
+	cmp(num: Fraction) {
+		if (!this.#denominator || !num.#denominator) {
+			return NaN;
+		}
+		const d = this.#numerator * num.#denominator - this.#denominator * num.#numerator;
+		return d < 0n ? -1 : d > 0n ? 1: 0;
+	}
+	isNaN() {
+		return !this.#denominator;
+	}
+	sign() {
+		return !this.#denominator ? NaN : !this.#numerator ? 0 : this.#numerator < 0n ? -1 : 1;
+	}
+	toFloat() {
+		if (!this.#denominator) {
+			return NaN;
+		}
+		return Number(10000n * this.#numerator / this.#denominator) / 10000;
+	}
+	static min(a: Fraction, b: Fraction) {
+		return a.cmp(b) === -1 ? a : b;
+	}
+	static max(a: Fraction, b: Fraction) {
+		return a.cmp(b) === 1 ? a : b;
+	}
+}
