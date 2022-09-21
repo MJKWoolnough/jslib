71e26d7c5d4eb9225b04e007a94694f84963a456
Michael Woolnough <michael.woolnough@gmail.com>
2022-09-21 19:45:48 +0100

    moved classes to top of file

--- lib.ts/css.ts
+++ lib.ts/css.ts
@@ -12,63 +12,6 @@ interface Def {
 
 type innerDef = Record<string, Value | ValueFn>;
 
-const normalise = (id: string) => {
-	id = id.trim();
-	let string = "";
-	for (let i = 0; i < id.length; i++) {
-		const c = id.charAt(i);
-		switch (c) {
-		case '"':
-		case "'":
-			string = c === string ? "" : c;
-			break;
-		case "\\":
-			i++;
-			break;
-		default:
-			if (!string && !c.trim()) {
-				let end = false;
-				for (let j = i + 1; j < id.length; j++ ) {
-					switch (id.charAt(j).trim()) {
-					case '+':
-					case '>':
-					case '~':
-					case '|':
-					case ',':
-					case '(':
-					case '[':
-						end = true;
-					case ')':
-					case ']':
-						id = id.slice(0, i) + id.slice(j);
-						i++;
-					case '':
-						break;
-					default:
-						if (i + 1 !== j) {
-							id = id.slice(0, i+1) + id.slice(j);
-						}
-					}
-				}
-				if (end) {
-					for (let j = i + 1; j < id.length; j++ ) {
-						if (id.charAt(j).trim()) {
-							if (i + 1 !== j) {
-								id = id.slice(0, i+1) + id.slice(j);
-							}
-							break;
-						}
-					}
-				}
-			}
-		}
-	}
-	return id;
-      },
-      isDef = (v: Value | Def | ValueFn): v is Def => Object.getPrototypeOf(v) === Object.prototype,
-      idRE = /^[_a-z0-9\-\240-\377]+/i,
-      classRE = /^\-?[_a-z\240-\377][_a-z0-9\-\240-\377]*$/i;
-
 export class CSS {
 	#data = new Map<string, innerDef>();
 	#idPrefix: string;
@@ -142,3 +85,60 @@ class Identifier {
 		return this.#ident;
 	}
 }
+
+const normalise = (id: string) => {
+	id = id.trim();
+	let string = "";
+	for (let i = 0; i < id.length; i++) {
+		const c = id.charAt(i);
+		switch (c) {
+		case '"':
+		case "'":
+			string = c === string ? "" : c;
+			break;
+		case "\\":
+			i++;
+			break;
+		default:
+			if (!string && !c.trim()) {
+				let end = false;
+				for (let j = i + 1; j < id.length; j++ ) {
+					switch (id.charAt(j).trim()) {
+					case '+':
+					case '>':
+					case '~':
+					case '|':
+					case ',':
+					case '(':
+					case '[':
+						end = true;
+					case ')':
+					case ']':
+						id = id.slice(0, i) + id.slice(j);
+						i++;
+					case '':
+						break;
+					default:
+						if (i + 1 !== j) {
+							id = id.slice(0, i+1) + id.slice(j);
+						}
+					}
+				}
+				if (end) {
+					for (let j = i + 1; j < id.length; j++ ) {
+						if (id.charAt(j).trim()) {
+							if (i + 1 !== j) {
+								id = id.slice(0, i+1) + id.slice(j);
+							}
+							break;
+						}
+					}
+				}
+			}
+		}
+	}
+	return id;
+      },
+      isDef = (v: Value | Def | ValueFn): v is Def => Object.getPrototypeOf(v) === Object.prototype,
+      idRE = /^[_a-z0-9\-\240-\377]+/i,
+      classRE = /^\-?[_a-z\240-\377][_a-z0-9\-\240-\377]*$/i;
