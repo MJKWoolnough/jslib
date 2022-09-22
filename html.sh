d83c55f3b9f9f6e483f7279602da8beee513cd7c
Michael Woolnough <michael.woolnough@gmail.com>
2022-09-22 16:52:45 +0100

    updated to use bindElement

--- html.sh
+++ html.sh
@@ -22,8 +22,8 @@ fi;
 
 if [ -n "$ts" ]; then
 	(
-		echo "import type {Children, DOMBind, Props} from './dom.js';";
-		echo "import {amendNode} from './dom.js';";
+		echo "import type {DOMBind} from './dom.js';";
+		echo "import {bindElement} from './dom.js';";
 		echo -en "\nexport const ns = \"http://www.w3.org/1999/xhtml\",\n[";
 		first=true;
 		for tag in $tags; do
@@ -38,7 +38,7 @@ if [ -n "$ts" ]; then
 				echo -n "$tag";
 			fi;
 		done;
-		echo -n "] = \"$tags\".split(\" \").map(e => (props?: Props | Children, children?: Children) => amendNode(document.createElementNS(ns, e), props, children)) as [";
+		echo -n "] = \"$tags\".split(\" \").map(e => bindElement(ns, e)) as [";
 		first=true;
 		for tag in $tags; do
 			if $first; then
@@ -54,7 +54,7 @@ fi;
 
 if [ -n "$js" ]; then
 	(
-		echo -e "import {amendNode} from './dom.js';\n";
+		echo -e "import {bindElement} from './dom.js';\n";
 		echo -en "export const ns = \"http://www.w3.org/1999/xhtml\",\n[";
 		first=true;
 		for tag in $tags; do
@@ -69,6 +69,6 @@ if [ -n "$js" ]; then
 				echo -n "$tag";
 			fi;
 		done;
-		echo "] = \"$tags\".split(\" \").map(e => (props, children) => amendNode(document.createElementNS(ns, e), props, children));";
+		echo "] = \"$tags\".split(\" \").map(e => bindElement(ns, e));";
 	) > "$js";
 fi;
