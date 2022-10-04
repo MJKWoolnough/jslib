659b3153b5d932a29b42bd44d465f97ea7bb1088
Michael Woolnough <michael.woolnough@gmail.com>
2022-10-04 15:48:07 +0100

    changed how the load module works. pageLoad is now default (and only) export, and include no longer exists

--- lib.ts/load.ts
+++ lib.ts/load.ts
@@ -1,12 +1,1 @@
-/*
-
-This file should be included in an HTML script element, and it creates two globally accessible features, which can be added to a TypeScript file with the following declarations
-
-declare const pageLoad: Promise<void>;
-declare const include: (url: string) => Promise<Object>;
-*/
-
-Object.defineProperties(window, {
-	"pageLoad": {value: document.readyState === "complete" ? Promise.resolve() : new Promise(successFn => window.addEventListener("load", successFn, {"once": true}))},
-	"include": {value: (url: string) => import(url)}
-});
+export default document.readyState === "complete" ? Promise.resolve() : new Promise(successFn => window.addEventListener("load", successFn, {"once": true}));
