84e9d6857be0a31458a5da36d7da0c8d0d8aa075
Michael Woolnough <michael.woolnough@gmail.com>
2022-10-24 13:13:13 +0100

    switched to using a single mutation observer object

--- lib.ts/elements.ts
+++ lib.ts/elements.ts
@@ -27,45 +27,48 @@ export interface Elem extends HTMLElement {
 	attr(name: string, def?: string): Bind<string>;
 }
 
+const attrs = new WeakMap<Node, Map<string, Bind<string> | AttrFn>>(),
+      attrObserver = new MutationObserver(list => {
+	for (const record of list) {
+		if (record.type === "attributes") {
+			const name = record.attributeName ?? "",
+			      ah = attrs.get(record.target)?.get(name);
+			if (ah) {
+				const v = (record.target as Elem).getAttribute(name);
+				if (ah instanceof Function) {
+					ah(v, record.oldValue);
+				} else {
+					ah.value = v ?? "";
+				}
+			}
+		}
+	}
+      });
+
 export default ((name: string, fn: (elem: Elem) => Children, options?: Options) => {
 	const shadowOptions: ShadowRootInit = {"mode": "closed", "slotAssignment": options?.manualSlot ? "manual" : "named", "delegatesFocus": options?.delegatesFocus ?? false},
 	      element = class extends (options?.removeEvent ? RemoveEvent : HTMLElement) {
-		#attrs: Map<string, Bind<string> | AttrFn>;
 		constructor() {
 			super();
-			this.#attrs = new Map();
-			new MutationObserver(list => {
-				for (const record of list) {
-					if (record.type === "attributes") {
-						const name = record.attributeName ?? "",
-							ah = this.#attrs.get(name);
-						if (ah) {
-							const v = this.getAttribute(name);
-							if (ah instanceof Function) {
-								ah(v, record.oldValue);
-							} else {
-								ah.value = v ?? "";
-							}
-						}
-					}
-				}
-			}).observe(this, {"attributeOldValue": true});
+			attrs.set(this, new Map());
+			attrObserver.observe(this, {"attributeOldValue": true});
 			amendNode(this.attachShadow(shadowOptions), fn(this));
 		}
 		attr(name: string, fn: AttrFn): void;
 		attr(name: string, def?: string): Bind<string>;
 		attr(name: string, fn?: string | AttrFn) {
-			if (this.#attrs.has(name)) {
+			const attrMap = attrs.get(this)!;
+			if (attrMap.has(name)) {
 				throw new Error("already assigned");
 			}
 			const v = this.getAttribute(name);
 			if (fn instanceof Function) {
-				this.#attrs.set(name, fn);
+				attrMap.set(name, fn);
 				fn(v, null);
 				return;
 			} else {
 				const b = bind<string>(v ?? fn ?? "");
-				this.#attrs.set(name, b);
+				attrMap.set(name, b);
 				return b;
 			}
 		}
