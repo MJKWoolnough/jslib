c0595bc48ac978c978e9abcaabe096c843f14a4d
Michael Woolnough <michael.woolnough@gmail.com>
2022-11-29 15:44:59 +0000

    added NodeArray->unshift tests

--- test/test.ts
+++ test/test.ts
@@ -5396,6 +5396,36 @@ type Tests = {
 					      removed = n.splice(1, 2);
 					return removed.length === 2 && removed[0] === items[1] && removed[1] === items[2] && n.length === 3 && n.at(0) === items[0] && n.at(2) === items[4];
 				}
+			},
+			"unshift": {
+				"empty unshift": async () => {
+					const {NodeArray, node} = await import("./lib/nodes.js"),
+					      item = {[node]: document.createElement("span")},
+					      n = new NodeArray(document.createElement("div"));
+					// @ts-ignore: Type Error (at least partially) caused by: https://github.com/microsoft/TypeScript/issues/35562
+					return n.length === 0 && n.unshift(item) === 1 && n.length === 1 && n.at(0) === item && n[node].children?.[0] === item[node];
+				},
+				"non-empty unshift": async () => {
+					const {NodeArray, node, noSort} = await import("./lib/nodes.js"),
+					      item = {[node]: document.createElement("span")},
+					// @ts-ignore: Type Error (at least partially) caused by: https://github.com/microsoft/TypeScript/issues/35562
+					      n = new NodeArray(document.createElement("div"), noSort, Array.from({length: 5}, (_, num) => ({[node]: document.createElement("span"), num})));
+					// @ts-ignore: Type Error (at least partially) caused by: https://github.com/microsoft/TypeScript/issues/35562
+					return n.length === 5 && n.unshift(item) === 6 && n.length === 6 && n.at(0) === item && n[node].children[0] === item[node];
+				},
+				"sorted unshift": async () => {
+					type MyNode = {
+						num: number;
+					}
+					const {NodeArray, node} = await import("./lib/nodes.js"),
+					// @ts-ignore: Type Error (at least partially) caused by: https://github.com/microsoft/TypeScript/issues/35562
+					      n = new NodeArray<MyNode>(document.createElement("div"), (a, b) => a.num - b.num);
+					// @ts-ignore: Type Error (at least partially) caused by: https://github.com/microsoft/TypeScript/issues/35562
+					for (let i = 0; i < 5; i++) {
+						n.unshift({[node]: document.createElement("span"), num: i});
+					}
+					return n.length === 5 && n.at(0)?.num === 0 && n.at(4)?.num === 4;
+				}
 			}
 		}
 	}
