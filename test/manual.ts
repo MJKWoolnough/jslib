5173edb0191a5b5195a9d08c0e871dfd69143e35
Michael Woolnough <michael.woolnough@gmail.com>
2022-12-03 14:52:49 +0000

    updated tests to always show failure button

--- test/manual.ts
+++ test/manual.ts
@@ -101,7 +101,7 @@ type ManualTests = {
 	});
 })({
 	"router": {
-		"simple non-match": [`import './lib/router.js';`, `<x-router><button route-match="" onclick="result(true)">Click here for success</button></x-router>`],
-		"simple match": [`import './lib/router.js';`, `<x-router><button route-match="/test" onclick="result(true)">Success</button><button route-match="" onclick="result(false)">Failed</button></x-router>`]
+		"simple non-match": [`import './lib/router.js';`, `<x-router><button route-match="" onclick="result(true)">Success</button></x-router><button onclick="result(false)">Click here if Success button isn't showing</button>`],
+		"simple match": [`import './lib/router.js';`, `<x-router><button route-match="/test" onclick="result(true)">Success</button></x-router><button onclick="result(false)">Click here if Succes button isn't showing</button>`]
 	}
 });
