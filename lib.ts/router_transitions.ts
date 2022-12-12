1e2f1fbaeb0cce9de9e432234192821e07232d94
Michael Woolnough <michael.woolnough@gmail.com>
2022-12-12 17:45:47 +0000

    factored out duped code into factory fn

--- lib.ts/router_transitions.ts
+++ lib.ts/router_transitions.ts
@@ -1,23 +1,17 @@
 import {registerTransition} from './router.js';
 
-registerTransition("fade", (current: ChildNode, next: ChildNode) => {
+const createTransition = (forCurrent: Keyframe[], forNext: Keyframe[], duration = 500) => (current: ChildNode, next: ChildNode) => {
 	if (current instanceof HTMLElement && next instanceof HTMLElement) {
 		const {offsetWidth, offsetHeight, offsetLeft, offsetTop} = current,
 		      currentAnim = new Animation(new KeyframeEffect(
 			current,
-			[
-				{"position": "absolute", "top": offsetTop + "px", "left": offsetLeft + "px", "width": offsetWidth + "px", "height": offsetHeight + "px", "opacity": 1},
-				{"position": "absolute", "top": offsetTop + "px", "left": offsetLeft + "px", "width": offsetWidth + "px", "height": offsetHeight + "px", "opacity": 0}
-			],
-			{"duration": 500}
+			forCurrent.map(k => Object.assign({"position": "absolute", "top": offsetTop + "px", "left": offsetLeft + "px", "width": offsetWidth + "px", "height": offsetHeight + "px"}, k)),
+			{duration}
 		      )),
 		      nextAnim = new Animation(new KeyframeEffect(
 			next,
-			[
-				{"opacity": 0},
-				{"opacity": 1},
-			],
-			{"duration": 500}
+			forNext,
+			{duration}
 		      ));
 		currentAnim.addEventListener("finish", () => current.remove(), {"once": true});
 		current.before(next);
@@ -26,32 +20,21 @@ registerTransition("fade", (current: ChildNode, next: ChildNode) => {
 	} else {
 		current.replaceWith(next);
 	}
-});
+      };
 
-registerTransition("wipe-left", (current: ChildNode, next: ChildNode) => {
-	if (current instanceof HTMLElement && next instanceof HTMLElement) {
-		const {offsetWidth, offsetHeight, offsetLeft, offsetTop} = current,
-		      currentAnim = new Animation(new KeyframeEffect(
-			current,
-			[
-				{"position": "absolute", "top": offsetTop + "px", "left": offsetLeft + "px", "width": offsetWidth + "px", "height": offsetHeight + "px", "clipPath": "inset(0 0 0 0)"},
-				{"position": "absolute", "top": offsetTop + "px", "left": offsetLeft + "px", "width": offsetWidth + "px", "height": offsetHeight + "px", "clipPath": "inset(0 100% 0 0)"}
-			],
-			{"duration": 500}
-		      )),
-		      nextAnim = new Animation(new KeyframeEffect(
-			next,
-			[
-				{"clipPath": "inset(0 0 0 100%)"},
-				{"clipPath": "inset(0 0 0 0)"},
-			],
-			{"duration": 500}
-		      ));
-		currentAnim.addEventListener("finish", () => current.remove(), {"once": true});
-		current.before(next);
-		currentAnim.play();
-		nextAnim.play();
-	} else {
-		current.replaceWith(next);
-	}
-});
+registerTransition("fade", createTransition([
+	{"opacity": 1},
+	{"opacity": 0}
+],
+[
+	{"opacity": 0},
+	{"opacity": 1}
+]));
+
+registerTransition("wipe-left", createTransition([
+	{"clipPath": "inset(0 0 0 0)"},
+	{"clipPath": "inset(0 100% 0 0)"}
+], [
+	{"clipPath": "inset(0 0 0 100%)"},
+	{"clipPath": "inset(0 0 0 0)"},
+]));
