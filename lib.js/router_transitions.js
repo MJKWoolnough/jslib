2e64843057790ca77e23c28a378d2c31a82a6e06
Michael Woolnough <michael.woolnough@gmail.com>
2022-12-12 18:14:01 +0000

    added JS version of router transitions

---
+++ lib.js/router_transitions.js
@@ -0,0 +1,59 @@
+import {registerTransition} from './router.js';
+
+export const createTransition = (forCurrent, forNext, duration = 500) => (current, next) => {
+	if (current instanceof HTMLElement && next instanceof HTMLElement) {
+		const {offsetWidth, offsetHeight, offsetLeft, offsetTop} = current,
+		      currentAnim = new Animation(new KeyframeEffect(
+			current,
+			forCurrent.map(k => Object.assign({"position": "absolute", "top": offsetTop + "px", "left": offsetLeft + "px", "width": offsetWidth + "px", "height": offsetHeight + "px"}, k)),
+			{duration}
+		      )),
+		      nextAnim = new Animation(new KeyframeEffect(
+			next,
+			forNext,
+			{duration}
+		      ));
+		currentAnim.addEventListener("finish", () => current.remove(), {"once": true});
+		current.before(next);
+		currentAnim.play();
+		nextAnim.play();
+	} else {
+		current.replaceWith(next);
+	}
+},
+fade = createTransition([
+	{"opacity": 1},
+	{"opacity": 0}
+],
+[
+	{"opacity": 0},
+	{"opacity": 1}
+]),
+wipeLeft = createTransition([
+	{"clipPath": "inset(0 0 0 0)"},
+	{"clipPath": "inset(0 100% 0 0)"}
+], [
+	{"clipPath": "inset(0 0 0 100%)"},
+	{"clipPath": "inset(0 0 0 0)"},
+]),
+wipeRight = createTransition([
+	{"clipPath": "inset(0 0 0 0)"},
+	{"clipPath": "inset(0 0 0 100%)"}
+], [
+	{"clipPath": "inset(0 100% 0 0)"},
+	{"clipPath": "inset(0 0 0 0)"},
+]),
+zoom = createTransition([
+	{"transform": "scale(1)"},
+	{"transform": "scale(0)"},
+	{"transform": "scale(0)"}
+], [
+	{"transform": "scale(0)"},
+	{"transform": "scale(0)"},
+	{"transform": "scale(1)"}
+]);
+
+registerTransition("fade", fade);
+registerTransition("wipe-left", wipeLeft);
+registerTransition("wipe-right", wipeRight);
+registerTransition("zoom", zoom);
