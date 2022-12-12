fac3f0989a4789ee4f2b5b016382a58d1d82abfb
Michael Woolnough <michael.woolnough@gmail.com>
2022-12-12 17:28:03 +0000

    added 'wipe-left' transition

--- lib.ts/router_transitions.ts
+++ lib.ts/router_transitions.ts
@@ -27,3 +27,31 @@ registerTransition("fade", (current: ChildNode, next: ChildNode) => {
 		current.replaceWith(next);
 	}
 });
+
+registerTransition("wipe-left", (current: ChildNode, next: ChildNode) => {
+	if (current instanceof HTMLElement && next instanceof HTMLElement) {
+		const {offsetWidth, offsetHeight, offsetLeft, offsetTop} = current,
+		      currentAnim = new Animation(new KeyframeEffect(
+			current,
+			[
+				{"position": "absolute", "top": offsetTop + "px", "left": offsetLeft + "px", "width": offsetWidth + "px", "height": offsetHeight + "px", "clipPath": "inset(0 0 0 0)"},
+				{"position": "absolute", "top": offsetTop + "px", "left": offsetLeft + "px", "width": offsetWidth + "px", "height": offsetHeight + "px", "clipPath": "inset(0 100% 0 0)"}
+			],
+			{"duration": 500}
+		      )),
+		      nextAnim = new Animation(new KeyframeEffect(
+			next,
+			[
+				{"clipPath": "inset(0 0 0 100%)"},
+				{"clipPath": "inset(0 0 0 0)"},
+			],
+			{"duration": 500}
+		      ));
+		currentAnim.addEventListener("finish", () => current.remove(), {"once": true});
+		current.before(next);
+		currentAnim.play();
+		nextAnim.play();
+	} else {
+		current.replaceWith(next);
+	}
+});
