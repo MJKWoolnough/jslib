5edca222c3c01111cd0ecda33894e456ba839e40
Michael Woolnough <michael.woolnough@gmail.com>
2022-12-12 16:32:17 +0000

    set 'current' node coords as part of animation (to avoid overwriting style settings)

--- lib.ts/router_transitions.ts
+++ lib.ts/router_transitions.ts
@@ -3,12 +3,11 @@ import {registerTransition} from './router.js';
 registerTransition("fade", (current: ChildNode, next: ChildNode) => {
 	if (current instanceof HTMLElement && next instanceof HTMLElement) {
 		const {offsetWidth, offsetHeight, offsetLeft, offsetTop} = current,
-		      s = current.getAttribute("style"),
 		      currentAnim = new Animation(new KeyframeEffect(
 			current,
 			[
-				{"opacity": 1},
-				{"opacity": 0}
+				{"position": "absolute", "top": offsetTop + "px", "left": offsetLeft + "px", "width": offsetWidth + "px", "height": offsetHeight + "px", "opacity": 1},
+				{"position": "absolute", "top": offsetTop + "px", "left": offsetLeft + "px", "width": offsetWidth + "px", "height": offsetHeight + "px", "opacity": 0}
 			],
 			{"duration": 500}
 		      )),
@@ -20,19 +19,7 @@ registerTransition("fade", (current: ChildNode, next: ChildNode) => {
 			],
 			{"duration": 500}
 		      ));
-		current.style.position = "absolute";
-		current.style.left = offsetLeft + "px";
-		current.style.top = offsetTop + "px";
-		current.style.width = offsetWidth + "px";
-		current.style.height = offsetHeight + "px";
-		currentAnim.addEventListener("finish", () => {
-			current.remove()
-			if (s) {
-				current.setAttribute("style", s);
-			} else {
-				current.removeAttribute("style");
-			}
-		}, {"once": true});
+		currentAnim.addEventListener("finish", () => current.remove(), {"once": true});
 		current.before(next);
 		currentAnim.play();
 		nextAnim.play();
