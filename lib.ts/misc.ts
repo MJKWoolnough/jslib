16227eee34e741f42f18c62022c1895a2668737e
Michael Woolnough <michael.woolnough@gmail.com>
2022-12-19 16:39:05 +0000

    checkInt first param should be 'any'

--- lib.ts/misc.ts
+++ lib.ts/misc.ts
@@ -1,3 +1,3 @@
 export const isInt = (v: any, min = -Infinity, max = Infinity): v is number => typeof v === "number" && (v|0) === v && v >= min && v <= max,
-checkInt = (n: number, min = -Infinity, max = Infinity, def = 0) => isInt(n, min, max) ? n : def,
+checkInt = (n: any, min = -Infinity, max = Infinity, def = 0) => isInt(n, min, max) ? n : def,
 mod = (n: number, m: number) => ((n % m) + m) % m;
