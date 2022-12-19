633d202fe2ce9b5481bef5041577a6f547a60532
Michael Woolnough <michael.woolnough@gmail.com>
2022-12-19 17:08:45 +0000

    added 'setAndReturn' function

--- lib.js/misc.js
+++ lib.js/misc.js
@@ -1,3 +1,7 @@
 export const isInt = (v, min = -Infinity, max = Infinity) => typeof v === "number" && (v|0) === v && v >= min && v <= max,
 checkInt = (n, min = -Infinity, max = Infinity, def = 0) => isInt(n, min, max) ? n : def,
-mod = (n, m) => ((n % m) + m) % m;
+mod = (n, m) => ((n % m) + m) % m,
+setAndReturn = (m, k, v) => {
+	m.set(k, v);
+	return v;
+};
--- lib.ts/misc.ts
+++ lib.ts/misc.ts
@@ -1,3 +1,7 @@
 export const isInt = (v: any, min = -Infinity, max = Infinity): v is number => typeof v === "number" && (v|0) === v && v >= min && v <= max,
 checkInt = (n: any, min = -Infinity, max = Infinity, def = 0) => isInt(n, min, max) ? n : def,
-mod = (n: number, m: number) => ((n % m) + m) % m;
+mod = (n: number, m: number) => ((n % m) + m) % m,
+setAndReturn = <K, V>(m: {set: (k: K, v: V) => any}, k: K, v: V) => {
+	m.set(k, v);
+	return v;
+};
