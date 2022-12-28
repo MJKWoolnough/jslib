e2c10be9082afe8d1dca26bf3cecbe949b3ee5ff
Michael Woolnough <michael.woolnough@gmail.com>
2022-12-28 10:47:39 +0000

    made typing more constrained

--- lib.ts/elements.ts
+++ lib.ts/elements.ts
@@ -37,12 +37,12 @@ type ConstructorOf<C> = {
 	new(...args: any[]): C;
 }
 
-interface OptionsFactory <T extends Node, U extends Options> {
+interface OptionsFactory <T extends HTMLElement | DocumentFragment, U extends Options> {
 	<V extends T>(fn: (elem: V) => Children, options?: U & {extend?: (base: ConstructorOf<T>) => ConstructorOf<V>, classOnly?: false}): DOMBind<V>;
 	<V extends T>(fn: (elem: V) => Children, options?: U & {extend?: (base: ConstructorOf<T>) => ConstructorOf<V>, classOnly: true}): ConstructorOf<V>;
 }
 
-type HTMLElementORDocumentFragmentFactory<T extends Node, U extends {psuedo?: boolean}> = OptionsFactory<T & AttrClass & ChildClass, Options & {attrs?: true, observeChildren?: true} & U> & OptionsFactory<T & ChildClass, Options & {attrs: false, observeChildren?: true} & U> & OptionsFactory<T & AttrClass, Options & {attrs?: true, observeChildren: false} & U> & OptionsFactory<T, Options & {attrs: false, observeChildren: false} & U>;
+type HTMLElementORDocumentFragmentFactory<T extends HTMLElement | DocumentFragment, U extends {psuedo?: boolean}> = OptionsFactory<T & AttrClass & ChildClass, Options & {attrs?: true, observeChildren?: true} & U> & OptionsFactory<T & ChildClass, Options & {attrs: false, observeChildren?: true} & U> & OptionsFactory<T & AttrClass, Options & {attrs?: true, observeChildren: false} & U> & OptionsFactory<T, Options & {attrs: false, observeChildren: false} & U>;
 
 type ElementFactory = HTMLElementORDocumentFragmentFactory<HTMLElement, {psuedo?: false}> & HTMLElementORDocumentFragmentFactory<DocumentFragment, {psuedo: true}>;
 
