b9ba346cf89bbbcd69e53ccb403847b74804fef6
Michael Woolnough <michael.woolnough@gmail.com>
2023-01-25 19:12:30 +0000

    checkInt and isInt should really be taking an 'unknown' type

--- lib.ts/misc.ts
+++ lib.ts/misc.ts
@@ -1,5 +1,5 @@
-export const isInt = (v: any, min = -Infinity, max = Infinity): v is number => typeof v === "number" && (v|0) === v && v >= min && v <= max,
-checkInt = (n: any, min = -Infinity, max = Infinity, def = 0) => isInt(n, min, max) ? n : def,
+export const isInt = (v: unknown, min = -Infinity, max = Infinity): v is number => typeof v === "number" && (v|0) === v && v >= min && v <= max,
+checkInt = (n: unknown, min = -Infinity, max = Infinity, def = 0) => isInt(n, min, max) ? n : def,
 mod = (n: number, m: number) => ((n % m) + m) % m,
 setAndReturn = <K, V>(m: {set: (k: K, v: V) => any}, k: K, v: V) => {
 	m.set(k, v);
