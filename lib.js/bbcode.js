2f2f398421acbec2861712cbfb8d09b72de234c1
Michael Woolnough <michael.woolnough@gmail.com>
2023-01-26 10:58:21 +0000

    added JSDocs

--- lib.js/bbcode.js
+++ lib.js/bbcode.js
@@ -1,3 +1,10 @@
+/**
+ * This module contains a full [BBCode](https://en.wikipedia.org/wiki/BBCode) parser, allowing for custom tags and text handling.
+ *
+ * @module bbcode
+ */
+/** */
+
 const parseText = function* (text) {
 	const tags = [];
 	let last = 0;
@@ -99,10 +106,45 @@ const parseText = function* (text) {
 	}
       };
 
-export const text = Symbol("text"),
+export const
+/**
+ * A {@link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Symbol | Symbol} used to indicate the text processor in the Parsers type passed to the (default) parsing function.
+ */
+text = Symbol("text"),
+/**
+ * Intended for tag parsers, this function determines if a token is an {@link OpenTag}.
+ *
+ * @param {OpenTag | CloseTag | string} t Tag to determine if it is an `OpenTag`.
+ *
+ * @return {boolean} `true` if `t` is an `OpenTag`.
+ */
 isOpenTag = t => typeof t === "object" && t.attr !== undefined,
+/**
+ * Intended for tag parsers, this function determines if a token is a {@link CloseTag}.
+ *
+ * @param {OpenTag | CloseTag | string} t Tag to determine if it is a `CloseTag`.
+ *
+ * @return {boolean} `true` if `t` is a `CloseTag`.
+ */
 isCloseTag = t => typeof t === "object" && t.attr === undefined,
+/**
+ * Intended for tag parsers, this function determines if a token is a `string`.
+ *
+ * @param {OpenTag | CloseTag | string} t Tag to determine if it is a `string`.
+ *
+ * @return {boolean} `true` if `t` is a `string`.
+ */
 isString = t => typeof t === "string",
+/**
+ * Intended for tag parsers, appends parse BBCode to the passed {@link https://developer.mozilla.org/en-US/docs/Web/API/Node | Node}.
+ *
+ * @param {Node} node         The {@link https://developer.mozilla.org/en-US/docs/Web/API/Node | Node} to append the parsed contents to.
+ * @param {Tokeniser} t       The Tokeniser to parse from.
+ * @param {Parsers} p         The object containing the parsers.
+ * @param {string} [closeTag] Optional string containing the name of the closing tag.
+ *
+ * @returns {Node} The passed in node.
+ */
 process = (node, t, p, closeTag) => {
 	while (true) {
 		const tk = t.next().value;
@@ -127,4 +169,41 @@ process = (node, t, p, closeTag) => {
 	return node;
 };
 
+/**
+ * Intended for tag parses, this defines an opening tag, with a possible attribute.
+ *
+ * @typedef {Object} OpenTag
+ * @property {string} tagName     This is the parsed tag name for the opening tag.
+ * @property {string | null} attr This is the parsed attribute, if one was provided, or `null` if one was not.
+ * @property {string} fullText    This is the full text of the parsed opening tag, including brackets and any attribute.
+ */
+
+/**
+ * Intended for tag parses, this defines a closing tag, with a possible attribute.
+ *
+ * @typedef {Object} CloseTag
+ * @property {string} tagName  This is the parsed tag name for the closing tag.
+ * @property {string} fullText This is the full text of the parsed closing tag, including brackets.
+ */
+
+/**
+ * Intended for tag parses, this type is a generator that will yield a token, which will either be a {@link CloseTag}, {@link OpenTag}, or string. When calling `next` on this Generator, you can pass in `true` to the `next` method retrieve the last token generated. If you pass in `1` to the `next` method, when it has just outputted an {@link OpenTag}, the processor will not move past the corresponding {@link CloseTag} until `1` is again passed to the `next` method.
+ *
+ * @typedef {Generator<OpenTag | CloseTag | string, void, true | 1 | undefined>} Tokeniser
+ */
+
+/**
+ * A function that takes a {@link https://developer.mozilla.org/en-US/docs/Web/API/Node | Node}, a {@link Tokeniser}, and a {@link Parsers} object. This function should process tokens from the {@link Tokeniser}, appending to the {@link https://developer.mozilla.org/en-US/docs/Web/API/Node | Node}, until its tag data finishes. This function should return nothing.
+ *
+ * @typedef {(node: Node, t: Tokeniser, p: Parsers) => void} TagFn
+ */
+
+/**
+ * This function parses the given text according, handling the tags with the given parsers, and appending all generated {@link https://developer.mozilla.org/en-US/docs/Web/API/Node | Node}s to a {@link https://developer.mozilla.org/en-US/docs/Web/API/DocumentFragment | DocumentFragment}, which is returned.
+ *
+ * @param {Parsers} parsers The bbcode tag parsers.
+ * @param {string} text     The text to be parsed.
+ *
+ * @return {DocumentFragment} DocumentFragment containing the parsed elements.
+ */
 export default (parsers, text) => process(document.createDocumentFragment(), parseText(text), parsers);
--- lib.ts/bbcode.ts
+++ lib.ts/bbcode.ts
@@ -1,3 +1,10 @@
+/**
+ * This module contains a full [BBCode](https://en.wikipedia.org/wiki/BBCode) parser, allowing for custom tags and text handling.
+ *
+ * @module bbcode
+ */
+/** */
+
 const parseText = function* (text: string): Tokeniser {
 	const tags: string[] = [];
 	let last = 0;
@@ -99,10 +106,46 @@ const parseText = function* (text: string): Tokeniser {
 	}
       };
 
-export const text = Symbol("text"),
+export const
+/**
+ * A {@link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Symbol | Symbol} used to indicate the text processor in the Parsers type passed to the (default) parsing function.
+ */
+text = Symbol("text"),
+/**
+ * Intended for tag parsers, this function determines if a token is an OpenTag.
+ *
+ * @param {OpenTag | CloseTag | string} t Tag to determine if it is an OpenTag.
+ *
+ * @return {boolean} `true` if `t` is an `OpenTag`.
+ */
 isOpenTag = (t: OpenTag | CloseTag | string): t is OpenTag => typeof t === "object" && (t as OpenTag).attr !== undefined,
+/**
+ * Intended for tag parsers, this function determines if a token is a CloseTag.
+ *
+ * @param {OpenTag | CloseTag | string} t Tag to determine if it is a CloseTag.
+ *
+ * @return {boolean} `true` if `t` is an `CloseTag`.
+ */
 isCloseTag = (t: OpenTag | CloseTag | string): t is CloseTag => typeof t === "object" && (t as OpenTag).attr === undefined,
+/**
+ * Intended for tag parsers, this function determines if a token is a string.
+ *
+ * @param {OpenTag | CloseTag | string} t Tag to determine if it is a CloseTag.
+ * 
+ * @return {boolean} `true` if `t` is an `string`.
+ */
 isString = (t: OpenTag | CloseTag | string): t is string => typeof t === "string",
+/**
+ * Intended for tag parsers, appends parse BBCode to the passed {@link https://developer.mozilla.org/en-US/docs/Web/API/Node | Node}.
+ *
+ * @typeParam {Node} T
+ * @param {T} node            The {@link https://developer.mozilla.org/en-US/docs/Web/API/Node | Node} to append the parsed contents to.
+ * @param {Tokeniser} t       The Tokeniser to parse from.
+ * @param {Parsers} p         The object containing the parsers.
+ * @param {string} [closeTag] Optional string containing the name of the closing tag.
+ *
+ * @return {T} The passed in node.
+ */
 process = <T extends Node>(node: T, t: Tokeniser, p: Parsers, closeTag?: string) => {
 	while (true) {
 		const tk = t.next().value;
@@ -127,24 +170,48 @@ process = <T extends Node>(node: T, t: Tokeniser, p: Parsers, closeTag?: string)
 	return node;
 };
 
+/** Intended for tag parses, this defines an opening tag, with a possible attribute. */
 export type OpenTag = {
+	/** This is the parsed tag name for the opening tag. */
 	tagName: string;
+	/** This is the parsed attribute, if one was provided, or `null` if one was not. */
 	attr: string | null;
+	/** This is the full text of the parsed opening tag, including brackets and any attribute. */
 	fullText: string;
 }
 
+/** Intended for tag parses, this defines a closing tag, with a possible attribute. */
 export type CloseTag = {
+	/** This is the parsed tag name for the closing tag. */
 	tagName: string;
+	/** This is the full text of the parsed opening tag, including brackets. */
 	fullText: string;
 }
 
+/**
+ * Intended for tag parses, this type is a generator that will yield a token, which will either be a {@link CloseTag}, {@link OpenTag}, or string. When calling `next` on this Generator, you can pass in `true` to the `next` method retrieve the last token generated. If you pass in `1` to the `next` method, when it has just outputted an {@link OpenTag}, the processor will not move past the corresponding {@link CloseTag} until `1` is again passed to the `next` method.
+ */
 export type Tokeniser = Generator<OpenTag | CloseTag | string, void, true | 1 | undefined>;
 
+/**
+ * A function that takes a {@link https://developer.mozilla.org/en-US/docs/Web/API/Node | Node}, a {@link Tokeniser}, and a {@link Parsers} object. This function should process tokens from the {@link Tokeniser}, appending to the {@link https://developer.mozilla.org/en-US/docs/Web/API/Node | Node}, until its tag data finishes. This function should return nothing.
+ */
 export type TagFn = (node: Node, t: Tokeniser, p: Parsers) => void;
 
+/**
+ * This type represents an Object, which contains the tag parsers for specific tags and the text processor. This object **must** contain the {@link text} {@link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Symbol | Symbol}, specifying a text formatting function, which takes a {@link https://developer.mozilla.org/en-US/docs/Web/API/Node | Node} to be appended to, and the string to be formatted. In addition, this object should contain string keys, which correspond to tag names, the values of which should be {@link TagFn}s.
+ */
 export type Parsers = {
 	[key: string]: TagFn;
 	[text]: (node: Node, t: string) => void;
 }
 
-export default (parsers: Parsers, text: string) => process(document.createDocumentFragment(), parseText(text), parsers);
+/**
+ * This function parses the given text according, handling the tags with the given parsers, and appending all generated {@link https://developer.mozilla.org/en-US/docs/Web/API/Node | Node}s to a {@link https://developer.mozilla.org/en-US/docs/Web/API/DocumentFragment | DocumentFragment}, which is returned.
+ *
+ * @param {Parsers} parsers The bbcode tag parsers.
+ * @param {string} text     The text to be parsed.
+ *
+ * @return {DocumentFragment} DocumentFragment containing the parsed elements.
+ */
+export default (parsers: Parsers, text: string): DocumentFragment => process(document.createDocumentFragment(), parseText(text), parsers);
