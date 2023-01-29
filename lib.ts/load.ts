7a475aa3d543519a8b3aa9c5780204dac167f385
Michael Woolnough <michael.woolnough@gmail.com>
2023-01-29 08:34:22 +0000

    added JSDocs

--- lib.ts/load.ts
+++ lib.ts/load.ts
@@ -1,1 +1,8 @@
+/**
+ * The load module contains a single default export, a {@link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise | Promise} which is resolved when the page finished loading.
+ * @module load
+ */
+/** */
+
+/** This Promise successfully resolves when the page is loaded. */
 export default document.readyState === "complete" ? Promise.resolve() : new Promise(successFn => window.addEventListener("load", successFn, {"once": true}));
