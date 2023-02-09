41a4e6f2cc7cc5b39fc1caf003a89172217d79f6
Michael Woolnough <michael.woolnough@gmail.com>
2023-02-09 09:19:39 +0000

    added JSDocs

--- lib.ts/router_transitions.ts
+++ lib.ts/router_transitions.ts
@@ -1,6 +1,24 @@
 import {registerTransition} from './router.js';
 
-export const createTransition = (forCurrent: Keyframe[], forNext = forCurrent.slice().reverse(), duration = 500) => (current: ChildNode, next: ChildNode) => {
+/**
+ * This library defines some simple transitional effects for the [router](#router) library.
+ *
+ * This module directly imports the {@link module:router}  module.
+ *
+ * @module router_transitions
+ * @requires {@link module:router}
+ */
+/** */
+
+export const
+/**
+ * This function creates simple transition function (as used with the {@link router:registerTransition | registerTranstition} function and the Router {@link router:Router/setTransition | setTransition} method of the router module.
+ *
+ * @param {Keyframe[]} forCurrent CSS Keyframes for the current Node.
+ * @param {Keyframe[]} [forNext]  CSS Ketframes for the next Node. If `forNext` is not specified, then it will be determined by reversing the `forCurrent` array.
+ * @param {number} [duration]     Total duration of the animation effect.
+ */
+createTransition = (forCurrent: Keyframe[], forNext = forCurrent.slice().reverse(), duration = 500) => (current: ChildNode, next: ChildNode) => {
 	if (current instanceof HTMLElement && next instanceof HTMLElement) {
 		const {offsetWidth, offsetHeight, offsetLeft, offsetTop} = current,
 		      currentAnim = new Animation(new KeyframeEffect(
@@ -19,10 +37,20 @@ export const createTransition = (forCurrent: Keyframe[], forNext = forCurrent.sl
 		current.replaceWith(next);
 	}
 },
+/**
+ * A simple fade transition.
+ *
+ * Can be used with the `x-router` tag by setting the `router-transition` attribute to `fade`.
+ */
 fade = createTransition([
 	{"opacity": 1},
 	{"opacity": 0}
 ]),
+/**
+ * A transition that reveals the new element beneath the first while wiping left.
+ *
+ * Can be used with the `x-router` tag by setting the `router-transition` attribute to `wipe-left`.
+ */
 wipeLeft = createTransition([
 	{"clipPath": "inset(0 0 0 0)"},
 	{"clipPath": "inset(0 100% 0 0)"}
@@ -30,6 +58,11 @@ wipeLeft = createTransition([
 	{"clipPath": "inset(0 0 0 100%)"},
 	{"clipPath": "inset(0 0 0 0)"}
 ]),
+/**
+ * A transition that reveals the new element beneath the first while wiping right.
+ *
+ * Can be used with the `x-router` tag by setting the `router-transition` attribute to `wipe-right`.
+ */
 wipeRight = createTransition([
 	{"clipPath": "inset(0 0 0 0)"},
 	{"clipPath": "inset(0 0 0 100%)"}
@@ -37,6 +70,11 @@ wipeRight = createTransition([
 	{"clipPath": "inset(0 100% 0 0)"},
 	{"clipPath": "inset(0 0 0 0)"}
 ]),
+/**
+ * A transition that scales out the first element before zooming in on the new element.
+ *
+ * Can be used with the `x-router` tag by setting the `router-transition` attribute to `zoom`.
+ */
 zoom = createTransition([
 	{"transform": "scale(1)"},
 	{"transform": "scale(0)"},
