13c202253bd99540cd210a7a96ee461768314707
Michael Woolnough <michael.woolnough@gmail.com>
2023-02-28 11:11:42 +0000

    added DOMException

--- test/node.ts
+++ test/node.ts
@@ -26,6 +26,108 @@
 		get: <T extends Node>(target: NodeList<T>, name: PropertyKey) => pIFn(name, index => target.item(index)) || (target as any)[name],
 	      };
 
+	class DOMException extends Error {
+		static readonly INDEX_SIZE_ERR = 1;
+		static readonly DOMSTRING_SIZE_ERR = 2;
+		static readonly HIERARCHY_REQUEST_ERR = 3;
+		static readonly WRONG_DOCUMENT_ERR = 4;
+		static readonly INVALID_CHARACTER_ERR = 5;
+		static readonly NO_DATA_ALLOWED_ERR = 6;
+		static readonly NO_MODIFICATION_ALLOWED_ERR = 7;
+		static readonly NOT_FOUND_ERR = 8;
+		static readonly NOT_SUPPORTED_ERR = 9;
+		static readonly INUSE_ATTRIBUTE_ERR = 10;
+		static readonly INVALID_STATE_ERR = 11;
+		static readonly SYNTAX_ERR = 12;
+		static readonly INVALID_MODIFICATION_ERR = 13;
+		static readonly NAMESPACE_ERR = 14;
+		static readonly INVALID_ACCESS_ERR = 15;
+		static readonly VALIDATION_ERR = 16;
+		static readonly TYPE_MISMATCH_ERR = 17;
+		static readonly SECURITY_ERR = 18;
+		static readonly NETWORK_ERR = 19;
+		static readonly ABORT_ERR = 20;
+		static readonly URL_MISMATCH_ERR = 21;
+		static readonly QUOTA_EXCEEDED_ERR = 22;
+		static readonly TIMEOUT_ERR = 23;
+		static readonly INVALID_NODE_TYPE_ERR = 24;
+		static readonly DATA_CLONE_ERR = 25;
+		readonly INDEX_SIZE_ERR = 1;
+		readonly DOMSTRING_SIZE_ERR = 2;
+		readonly HIERARCHY_REQUEST_ERR = 3;
+		readonly WRONG_DOCUMENT_ERR = 4;
+		readonly INVALID_CHARACTER_ERR = 5;
+		readonly NO_DATA_ALLOWED_ERR = 6;
+		readonly NO_MODIFICATION_ALLOWED_ERR = 7;
+		readonly NOT_FOUND_ERR = 8;
+		readonly NOT_SUPPORTED_ERR = 9;
+		readonly INUSE_ATTRIBUTE_ERR = 10;
+		readonly INVALID_STATE_ERR = 11;
+		readonly SYNTAX_ERR = 12;
+		readonly INVALID_MODIFICATION_ERR = 13;
+		readonly NAMESPACE_ERR = 14;
+		readonly INVALID_ACCESS_ERR = 15;
+		readonly VALIDATION_ERR = 16;
+		readonly TYPE_MISMATCH_ERR = 17;
+		readonly SECURITY_ERR = 18;
+		readonly NETWORK_ERR = 19;
+		readonly ABORT_ERR = 20;
+		readonly URL_MISMATCH_ERR = 21;
+		readonly QUOTA_EXCEEDED_ERR = 22;
+		readonly TIMEOUT_ERR = 23;
+		readonly INVALID_NODE_TYPE_ERR = 24;
+		readonly DATA_CLONE_ERR = 25;
+		static ERROR_NAMES = Object.freeze([
+			"",
+			"IndexSizeError",
+			"DOMSTRING_SIZE_ERR",
+			"HierarchyRequestError",
+			"WrongDocumentError",
+			"InvalidCharacterError",
+			"NO_DATA_ALLOWED_ERR",
+			"NoModificationAllowedError",
+			"NotFoundError",
+			"NotSupportedError",
+			"INUSE_ATTRIBUTE_ERR",
+			"InUseAttributeError",
+			"SyntaxError",
+			"InvalidModificationError",
+			"NamespaceError",
+			"InvalidAccessError",
+			"VALIDATION_ERR",
+			"TypeMismatchError",
+			"SecurityError",
+			"NetworkError",
+			"AbortError",
+			"URLMismatchError",
+			"QuotaExceededError",
+			"TimeoutError",
+			"InvalidNodeTypeError",
+			"DataCloneError",
+		]);
+		#code: number;
+		#message: string;
+		#name: string;
+		constructor(message = "", name = "") {
+			super(message);
+			this.#message = message;
+			this.#name = name;
+			this.#code = DOMException.ERROR_NAMES.indexOf(name);
+		}
+		get code() {
+			return this.#code;
+		}
+		get message() {
+			return this.#message;
+		}
+		get name() {
+			return this.#name;
+		}
+		[Symbol.toStringTag]() {
+			return "DOMException";
+		}
+	}
+
 	class Node extends EventTarget {
 		static readonly ELEMENT_NODE = 1;
 		static readonly ATTRIBUTE_NODE = 2;
