1789611ff2d1ca905ee4f12b1802462b8c1138c6
Michael Woolnough <michael.woolnough@gmail.com>
2023-07-29 16:23:44 +0100

    start of typeguard module

---
+++ lib.ts/typeguard.ts
@@ -0,0 +1,9 @@
+export const Bool = () => (v: unknown): v is boolean => typeof v === "boolean",
+Str = () => (v: unknown): v is string => typeof v === "string",
+Undefined = () => (v: unknown): v is undefined => v === undefined,
+Null = () => (v: unknown): v is null => v === null,
+Num = () => (v: unknown): v is number => typeof v === "number",
+BigInt = () => (v: unknown): v is bigint => typeof v === "bigint",
+Sym = () => (v: unknown): v is Symbol => typeof v === "symbol",
+Arr = () => (v: unknown): v is Array<any> => v instanceof Array,
+Obj = () => (v: unknown): v is Object => v instanceof Object;
