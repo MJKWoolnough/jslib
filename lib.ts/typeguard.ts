5e8081649c7e366a7366dc557fb6db97a198a006
Michael Woolnough <michael.woolnough@gmail.com>
2023-09-06 06:47:49 +0100

    Val should allow bigints

--- lib.ts/typeguard.ts
+++ lib.ts/typeguard.ts
@@ -194,7 +194,7 @@ Sym = () => asTypeGuard((v: unknown): v is symbol => throwOrReturn(typeof v === 
  *
  * @return {TypeGuard<any>}
  */
-Val = <const T extends boolean | number | string | null | undefined>(val: T) => asTypeGuard((v: unknown): v is T => throwOrReturn(v === val, "value")),
+Val = <const T extends boolean | number | bigint | string | null | undefined>(val: T) => asTypeGuard((v: unknown): v is T => throwOrReturn(v === val, "value")),
 /**
  * The Any function returns a TypeGuard that allows any value.
  *
