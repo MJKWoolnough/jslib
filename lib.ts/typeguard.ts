d736dcee727703251d62e9371563256c03dbeb18
Michael Woolnough <michael.woolnough@gmail.com>
2023-09-24 17:20:00 +0100

    updated to simplified definition for Tuple

--- lib.ts/typeguard.ts
+++ lib.ts/typeguard.ts
@@ -550,7 +550,7 @@ Tuple = <const T extends readonly any[], const U extends {[K in keyof T]: TypeGu
 		}
 
 		return throwOrReturn(pos === v.length, "tuple", "", "extra values");
-	}, ["Tuple", tgs, spread]);
+	}, () => ["Tuple", tgs.map(tg => tg.def()), spread?.def()]);
 },
 /**
  * The Obj function returns a TypeGuard that checks for an object type defined by the passed object of TypeGuards.
