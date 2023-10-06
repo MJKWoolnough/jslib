747ba15e67884c70a531eedbb21d6d4b28c25e41
Michael Woolnough <michael.woolnough@gmail.com>
2023-10-06 08:04:08 +0100

    reduced Object stringification code

--- lib.ts/typeguard.ts
+++ lib.ts/typeguard.ts
@@ -200,21 +200,7 @@ const throwUnknownError = (v: boolean) => {
 	case "Or":
 		return (def[1] as Definition[]).map(d => def[0] === "And" && d[0] === "Or" ? `(${toString(d)})` : toString(d)).join(def[0] === "And" ? " & " : " | ");
 	case "Object":
-		let arr = "{";
-
-		for (const [k, d] of Object.entries(def[1]) as [string, Definition][]) {
-			if (typeof k === "string") {
-				const hasUndefined = (d[0] === "" && d[1] === "undefined") || d[0] === "Or" && (d[1] as Definition[]).some(e => e[0] === "" && e[1] === "undefined");
-
-				arr += `\n	${k.match(identifer) ? k : JSON.stringify(k)}${hasUndefined ? "?" : ""}: ${toString(d).replaceAll("\n", "\n	")};`;
-			}
-		}
-
-		if (arr.length > 1) {
-			arr += "\n";
-		}
-
-		return arr + "}";
+		return (Object.entries(def[1]) as [string, Definition][]).filter(([k]) => typeof k === "string").map(([k, d]) => `	${k.match(identifer) ? k : JSON.stringify(k)}${(d[0] === "" && d[1] === "undefined") || d[0] === "Or" && (d[1] as Definition[]).some(e => e[0] === "" && e[1] === "undefined") ? "?" : ""}: ${toString(d).replaceAll("\n", "\n	")};\n`).reduce((t, e, n) => t + (!n ? "\n" : "") + e, "{")  + "}";
 	case "Tuple":
 		return "[" + (def[1] as Definition[]).map(toString).concat(def[2] ? "..." + (["Or", "And"].includes(def[2][0]) ? `(${toString(def[2])})` : toString(def[2])) + "[]" : []).join(", ") + "]";
 	default:
