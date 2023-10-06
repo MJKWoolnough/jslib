6db45fd01c52fff1d13fa8e829eaf367b622a691
Michael Woolnough <michael.woolnough@gmail.com>
2023-10-06 06:54:46 +0100

    reduce And/Or stringification code

--- lib.ts/typeguard.ts
+++ lib.ts/typeguard.ts
@@ -200,11 +200,7 @@ const throwUnknownError = (v: boolean) => {
 		return `${isGroup ? "(" : ""}${toString(def[1])}${isGroup ? ")" : ""}[]`;
 	case "And":
 	case "Or":
-		return (def[1] as Definition[]).map(d => {
-			const group = def[0] === "And" && d[0] === "Or";
-
-			return `${group ? "(" : ""}${toString(d)}${group ? ")" : ""}`;
-		}).join(def[0] === "And" ? " & " : " | ");
+		return (def[1] as Definition[]).map(d => def[0] === "And" && d[0] === "Or" ? `(${toString(d)})` : toString(d)).join(def[0] === "And" ? " & " : " | ");
 	case "Object":
 		let arr = "{";
 
