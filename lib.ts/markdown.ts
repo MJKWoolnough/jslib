7df39306f026c036777ed57ad08ff92ea5eea685
Michael Woolnough <michael.woolnough@gmail.com>
2023-11-04 19:22:14 +0000

    added heading parsing

--- lib.ts/markdown.ts
+++ lib.ts/markdown.ts
@@ -1,9 +1,16 @@
-import {p, hr} from './html.js';
+import {p, h1, h2, h3, h4, h5, h6, hr} from './html.js';
 
 const tags = {
 	"paragraphs": p,
 	"thematicBreaks": hr,
+	"heading1": h1,
+	"heading2": h2,
+	"heading3": h3,
+	"heading4": h4,
+	"heading5": h5,
+	"heading6": h6,
       } as const,
+      isHeading = /^ {0,3}#{1,6}( .*)?$/,
       isThematicBreak = [
 	      /^ {0,3}(\-[ 	]*){3,}[ 	]*$/,
 	      /^ {0,3}(\*[ 	]*){3,}[ 	]*$/,
@@ -31,6 +38,14 @@ class Markdown {
 
 		Loop:
 		for (const line of markdown.split("\n")) {
+			if (line.match(isHeading)) {
+				const t = line.trimStart(),
+				      start = t.indexOf(" ") as 1 | 2 | 3 | 4 | 5 | 6;
+
+				pushBlock(tags[`heading${start}`](this.parseInline(t.slice(start).trim())));
+
+				continue Loop;
+			}
 			for (const tb of isThematicBreak) {
 				if (line.match(tb)) {
 					pushBlock(tags.thematicBreaks());
