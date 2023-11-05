4bec769576967c0110a929dbda8ac9a174cae34a
Michael Woolnough <michael.woolnough@gmail.com>
2023-11-05 18:24:41 +0000

    escaped hashes don't count as end-of-heading hashes

--- lib.ts/markdown.ts
+++ lib.ts/markdown.ts
@@ -42,7 +42,7 @@ class Markdown {
 				const t = line.trimStart(),
 				      start = t.indexOf(" ") as -1 | 1 | 2 | 3 | 4 | 5 | 6;
 
-				pushBlock(tags[`heading${start === -1 ? t.length as 1 | 2 | 3 | 4 | 5 | 6 : start}`](this.parseInline(start === -1 ? "" : t.slice(start).replace(/#*$/, "").trim())));
+				pushBlock(tags[`heading${start === -1 ? t.length as 1 | 2 | 3 | 4 | 5 | 6 : start}`](this.parseInline(start === -1 ? "" : t.slice(start).replace(/(\\#)?#*$/, "$1").replace("\\#", "#").trim())));
 
 				continue Loop;
 			}
