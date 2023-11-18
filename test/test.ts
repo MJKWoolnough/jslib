affc70192e6ad3604af010add54310c83bba07a3
Michael Woolnough <michael.woolnough@gmail.com>
2023-11-18 20:17:00 +0000

    added test for block quotes being seperated by a blank line

--- test/test.ts
+++ test/test.ts
@@ -9844,7 +9844,8 @@ type Tests = {
 				["   > # Foo\n   > bar\n > baz", "<blockquote><h1>Foo</h1><p>bar\nbaz</p></blockquote>"],
 				[" >", "<blockquote></blockquote>"],
 				[" >\n>  \n> ", "<blockquote></blockquote>"],
-				[">\n> foo\n>  ", "<blockquote><p>foo</p></blockquote>"]
+				[">\n> foo\n>  ", "<blockquote><p>foo</p></blockquote>"],
+				["> foo\n\n> bar", "<blockquote><p>foo</p></blockquote><blockquote><p>bar</p></blockquote>"],
 			],
 			"not a block quote": [
 				["    > # Foo\n    > bar\n    > baz", "<pre><code>&gt; # Foo\n&gt; bar\n&gt; baz</code></pre>"]
