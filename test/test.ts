e1bd836c156b0aebb880c8156cc0d4576ab46243
Michael Woolnough <michael.woolnough@gmail.com>
2023-11-21 07:53:53 +0000

    added a not-a-code-block test

--- test/test.ts
+++ test/test.ts
@@ -9813,6 +9813,9 @@ type Tests = {
 				["a paragraph\n\n    a simple\n      indented code block", "<p>a paragraph</p><pre><code>a simple\n  indented code block</code></pre>"],
 				["    a simple\n      indented code block\na paragraph", "<pre><code>a simple\n  indented code block\n</code></pre><p>a paragraph</p>"],
 				["# Heading\n    foo\nHeading\n------\n    foo\n----", "<h1>Heading</h1><pre><code>foo\n</code></pre><h2>Heading</h2><pre><code>foo\n</code></pre><hr>"]
+			],
+			"not a code block": [
+				["Foo\n    bar", "<p>Foo\nbar</p>"]
 			]
 		},
 		"fenced code blocks": {
