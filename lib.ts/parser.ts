90c59eb0fe8c542ddde1ec65eaa6803e9c9ab603
Michael Woolnough <michael.woolnough@gmail.com>
2023-11-24 20:29:55 +0000

    added basic parsing methods

--- lib.ts/parser.ts
+++ lib.ts/parser.ts
@@ -12,9 +12,33 @@ type ParserFn = (p: Parser) => [Token, ParserFn];
 
 class Parser {
 	#text: string;
+	#pos = 0;
+	#lastPos = 0;
 
 	constructor(text: string) {
 		this.#text = text;
+	}
+
+	#next() {
+		if (this.#pos === this.#text.length) {
+			return -1;
+		}
+
+		return this.#text.charAt(this.#pos++);
+	}
+
+	#backup() {
+		if (this.#pos > this.#lastPos) {
+			this.#pos--;
+		}
+	}
+
+	get() {
+		const str = this.#text.slice(this.#lastPos, this.#pos);
+
+		this.#lastPos = this.#pos;
+
+		return str;
 	}
 }
 
