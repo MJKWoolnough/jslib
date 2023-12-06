05b3ef0344e676963a81208997d8d006d94d97ba
Michael Woolnough <michael.woolnough@gmail.com>
2023-12-06 19:51:22 +0000

    added tests for Phraser.next method

--- test/test.ts
+++ test/test.ts
@@ -9960,6 +9960,26 @@ type Tests = {
 					return peeked;
 				}
 			},
+			"next": {
+				"12345abcde": async () => {
+					const {default: parser} = await import("./lib/parser.js"),
+					      p = parser("", p => [{"type": 1, "data": "12345"}, () => [{"type": 2, "data": "abcde"}, () => p.done()]], p => {
+						return [{"type": +(p.next() === 1 && p.next() === 2), "data": p.get()}, () => p.done()];
+					      }).next().value;
+
+					return JSON.stringify(p) === `{"type":1,"data":[{"type":1,"data":"12345"},{"type":2,"data":"abcde"}]}`;
+				},
+				"abcde": async () => {
+					const {default: parser} = await import("./lib/parser.js"),
+					      p = parser("", p => [{"type": 2, "data": "abcde"}, () => p.done()], p => {
+						return [{"type": 2 + +(p.next() === 2 && p.next() === -1), "data": p.get()}, () => p.done()];
+					      }).next().value;
+
+					console.log(p);
+
+					return JSON.stringify(p) === `{"type":3,"data":[{"type":2,"data":"abcde"}]}`;
+				}
+			},
 			"accept": {
 				"12345abcde": async () => {
 					const {default: parser} = await import("./lib/parser.js"),
