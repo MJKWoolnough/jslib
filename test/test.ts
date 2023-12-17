fb9305dba60a38361ca57d1a1d7d9ecea1d10f57
Michael Woolnough <michael.woolnough@gmail.com>
2023-12-17 20:43:34 +0000

    removed invalid test

--- test/test.ts
+++ test/test.ts
@@ -10710,7 +10710,6 @@ type Tests = {
 				["Foo\n\nbar\n---\nbaz", "<p>Foo</p><h2>bar</h2><p>baz</p>"],
 				["Foo\nbar\n\n---\nbaz", "<p>Foo\nbar</p><hr><p>baz</p>"],
 				["Foo\nbar\n\\---\nbaz", "<p>Foo\nbar\n---\nbaz</p>"],
-				["`Foo\n----\n`\n\n<a title=\"a lot\n---\nof dashes\"/>", "<h2>`Foo</h2><p>`</p><h2>&lt;a title=\"a lot</h2><p>of dashes\"/&gt;</p>"],
 				["---\nFoo\n---\nBar\n---\nBaz", "<hr><h2>Foo</h2><h2>Bar</h2><p>Baz</p>"]
 			]
 		},
