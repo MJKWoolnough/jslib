f090d08746fa5b7bfcb946a4689f6099749582eb
Michael Woolnough <michael.woolnough@gmail.com>
2024-01-27 20:57:25 +0000

    added superscript tests with complex escaping

--- test/test.ts
+++ test/test.ts
@@ -11254,7 +11254,9 @@ type Tests = {
 				["^foo\\^", "<p>^foo^</p>"],
 				["2^4 + 3^5", "<p>2^4 + 3^5</p>"],
 				["^foo~bar^baz^bar~foo^", "<p><sup>foo~bar</sup>baz<sup>bar~foo</sup></p>"],
-				["^\\ foo\\ ^", "<p><sup> foo </sup></p>"]
+				["^\\ foo\\ ^", "<p><sup> foo </sup></p>"],
+				["^foo\\\\\\\\\\\\\\ bar^", "<p><sup>foo\\\\\\ bar</sup></p>"],
+				["^foo\\\\\\\\\\\\ bar^", "<p>^foo\\\\\\ bar^</p>"]
 			],
 			"strikethrough": [
 				["~~Strikeout~~", "<p><s>Strikeout</s></p>"],
