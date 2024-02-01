9710c69f33bb7ef480abb632128f619e28b79272
Michael Woolnough <michael.woolnough@gmail.com>
2024-02-01 18:49:36 +0000

    added mark tests with mark disabled

--- test/test.ts
+++ test/test.ts
@@ -11528,6 +11528,12 @@ type Tests = {
 				["x ++++foo++ bar++", "<p>x ++++foo++ bar++</p>"],
 				["x ++foo ++bar++++", "<p>x ++foo ++bar++++</p>"],
 				["x ++++foo++++", "<p>x ++++foo++++</p>"]
+			],
+			"mark": [
+				["==Mark==", "<p>==Mark==</p>"],
+				["x ====foo== bar==", "<p>x ====foo== bar==</p>"],
+				["x ==foo ==bar====", "<p>x ==foo ==bar====</p>"],
+				["x ====foo====", "<p>x ====foo====</p>"]
 			]
 		}
 	} as Record<string, Record<string, [string, string][]>>).reduce((o, [title, tests]) => (o[title] = Object.entries(tests).reduce((p, [subtitle, testArr]) => (p[subtitle] = testArr.reduce((q, [input, output], n) => (q[n+1] = Object.defineProperty(async () => {
