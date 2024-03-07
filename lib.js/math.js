2a2fd4548599c13877e3990e85dfd45408f44f85
Michael Woolnough <michael.woolnough@gmail.com>
2024-03-07 20:55:16 +0000

    regen'd

--- lib.js/math.js
+++ lib.js/math.js
@@ -10,6 +10,6 @@ import {bindElement} from './dom.js';
 /** */
 
 export const
-/** This constant contains the XMLNamespace of HTMLElements. */
+/** This constant contains the XMLNamespace of MathMLElements. */
 ns = "http://www.w3.org/1998/Math/MathML",
 [annotation, annotationXML, maction, math, menclose, merror, mfenced, mfrac, mi, mmultiscripts, mn, mo, mover, mpadded, mphantom, mprescripts, mroot, mrow, ms, mspace, msqrt, mstyle, msub, msubsup, msup, mtable, mtd, mtext, mtr, munder, munderover, semantics] = "annotation annotation-xml maction math menclose merror mfenced mfrac mi mmultiscripts mn mo mover mpadded mphantom mprescripts mroot mrow ms mspace msqrt mstyle msub msubsup msup mtable mtd mtext mtr munder munderover semantics".split(" ").map(e => bindElement(ns, e));
