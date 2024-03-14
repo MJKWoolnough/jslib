286ec09669af3f071d8c1560420727ac51d53c94
Michael Woolnough <michael.woolnough@gmail.com>
2024-03-14 20:47:19 +0000

    add simple 'setData' method to populate data-table

--- lib.ts/data_table.ts
+++ lib.ts/data_table.ts
@@ -1,9 +1,13 @@
-import {bindElement} from './dom.js';
-import {ns} from './html.js';
+import {bindElement, clearNode} from './dom.js';
+import {tbody, td, tr, ns} from './html.js';
 
 export class DataTable extends HTMLTableElement {
 	constructor() {
 		super();
+	}
+
+	setData(data: (string | number | boolean)[][]) {
+		clearNode(this, tbody(data.map(row => tr(row.map(cell => td(cell+""))))));
 	}
 }
 
