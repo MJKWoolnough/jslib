dd18c922cf846ec390b69970ae786af48b33d755
Michael Woolnough <michael.woolnough@gmail.com>
2024-03-14 21:04:02 +0000

    change to using NodeArrays for rows and cells

--- lib.ts/data_table.ts
+++ lib.ts/data_table.ts
@@ -1,15 +1,39 @@
-import {amendNode, bindElement, clearNode} from './dom.js';
-import {slot, table, tbody, td, tr, ns} from './html.js';
+import {amendNode, bindElement, child} from './dom.js';
+import {table, tbody, td, tr, ns} from './html.js';
+import {NodeArray} from './nodes.js';
+
+type Value = string | number | boolean;
+
+type Cell = {
+	[child]: HTMLTableCellElement;
+	value: Value;
+}
 
 export class DataTable extends HTMLElement {
+	#body: NodeArray<NodeArray<Cell>>;
 	constructor() {
 		super();
 
-		amendNode(this.attachShadow({"mode": "closed"}), table(slot()));
+		this.#body = new NodeArray(tbody());
+
+		amendNode(this.attachShadow({"mode": "closed"}), table(this.#body));
 	}
 
-	setData(data: (string | number | boolean)[][]) {
-		clearNode(this, tbody(data.map(row => tr(row.map(cell => td(cell+""))))));
+	setData(data: Value[][]) {
+		this.#body.splice(0, this.#body.length);
+
+		for (const row of data) {
+			const rowArr = new NodeArray<Cell>(tr())
+
+			for (const cell of row) {
+				rowArr.push({
+					[child]: td(cell + ""),
+					value: cell
+				});
+			}
+
+			this.#body.push(rowArr);
+		}
 	}
 }
 
