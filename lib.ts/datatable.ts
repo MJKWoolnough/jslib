baf0c454eb9ae84a5d10be42276b1f4afc2557f0
Michael Woolnough <michael.woolnough@gmail.com>
2024-03-25 20:42:42 +0000

    added types to enable programatic changint of filters

--- lib.ts/datatable.ts
+++ lib.ts/datatable.ts
@@ -48,6 +48,25 @@ type Header = {
 	col: number;
 }
 
+type HeaderFilter = {
+	[child]: HTMLUListElement;
+	primary: HTMLInputElement;
+	nonEmpty?: HTMLInputElement;
+	empty?: HTMLInputElement;
+}
+
+type TextHeaderFilter = HeaderFilter & {
+	text: HTMLInputElement;
+	isPrefix: HTMLButtonElement;
+	isSuffix: HTMLButtonElement;
+	isCaseInsensitive: HTMLButtonElement;
+}
+
+type NumberHeaderFilter = HeaderFilter & {
+	min: HTMLInputElement;
+	max: HTMLInputElement;
+}
+
 const arrow = (up: 0 | 1) => `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 40 20'%3E%3Cpath d='M1,${19 - 18 * up}h38l-19,${(2 * up - 1) * 18}z' fill='%23f00' stroke='%23000' stroke-linejoin='round' /%3E%3C/svg%3E%0A")`,
       style = [
 	new CSS().add({
