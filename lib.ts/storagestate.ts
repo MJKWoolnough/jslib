a0749367fce21f96096d90fe6bf5464e6ec6ce3f
Michael Woolnough <michael.woolnough@gmail.com>
2024-04-09 20:52:17 +0100

    store reference to StorageBound on a map to be used when a storage event is fired.

--- lib.ts/storagestate.ts
+++ lib.ts/storagestate.ts
@@ -1,10 +1,13 @@
 import {Binding} from './bind.js';
+import {setAndReturn} from './misc.js';
 
 class StorageBound<T> extends Binding<T> {
 	#storage: Storage;
 	#name: string;
 	#typeguard: (v: unknown) => v is T;
 	#default: T;
+
+	static #listeners = new Map<Storage, StorageBound<any>[]>();
 
 	constructor(storage: Storage, name: string, value: T, typeguard: (v: unknown) => v is T) {
 		const val = JSON.parse(storage.getItem(name) ?? "");
@@ -15,6 +18,8 @@ class StorageBound<T> extends Binding<T> {
 		this.#name = name;
 		this.#default = value;
 		this.#typeguard = typeguard;
+
+		(StorageBound.#listeners.get(storage) ?? setAndReturn(StorageBound.#listeners, storage, [])).push(this);
 	}
 
 	get name() {
