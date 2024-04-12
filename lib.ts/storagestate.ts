85b048538317c7760749aa379cd6261dfbf527fb
Michael Woolnough <michael.woolnough@gmail.com>
2024-04-12 18:04:44 +0100

    add JSDocs

--- lib.ts/storagestate.ts
+++ lib.ts/storagestate.ts
@@ -1,3 +1,12 @@
+/**
+ * The storagestate module is used to create {@link bind:Binding | Binding}s that bind to Storage objects.
+ *
+ * @module storagestate
+ * @requires module:bind
+ * @requires module:misc
+ */
+/** */
+
 import {Binding} from './bind.js';
 import {setAndReturn} from './misc.js';
 
@@ -64,5 +73,30 @@ const parseJSON = (val: string) => {
 	}
 }
 
-export const bindLocalStorage = <T>(name: string, value: NoInfer<T>, typeguard: (v: unknown) => v is T = (_: unknown): _ is any => true) => new StorageBound(window.localStorage, name, value, typeguard),
+export const
+/**
+ * This function creates a {@link binding:Binding | Binding} that retrieves it's value from localStorage, and stores it's value in localStorage when set.
+ *
+ * The value automatically updates when another localStorage Binding Object with the same name is updated, or when the localStorage is set from another browsing context.
+ *
+ * @typedef T
+ * @param {string}                 name        The key used to store and retrieve from localStorage.
+ * @param {T}                      value       The default value that is used when a value retrieved from localStorage doesn't succeed the TypeGuard check.
+ * @param {(v: unknown) => v is T} [typeguard] A TypeGuard to confirm the parsed value is of the correct type.
+ *
+ * @returns {Binding<T>}
+ */
+bindLocalStorage = <T>(name: string, value: NoInfer<T>, typeguard: (v: unknown) => v is T = (_: unknown): _ is any => true) => new StorageBound(window.localStorage, name, value, typeguard),
+/**
+ * This function creates a {@link binding:Binding | Binding} that retrieves it's value from sessionStorage, and stores it's value in sessionStorage when set.
+ *
+ * The value automatically updates when another sessionStorage Binding Object with the same name is updated, or when the sessionStorage is set from another browsing context.
+ *
+ * @typedef T
+ * @param {string}                 name        The key used to store and retrieve from sessionStorage.
+ * @param {T}                      value       The default value that is used when a value retrieved from sessionStorage doesn't succeed the TypeGuard check.
+ * @param {(v: unknown) => v is T} [typeguard] A TypeGuard to confirm the parsed value is of the correct type.
+ *
+ * @returns {Binding<T>}
+ */
 bindSessionStorage = <T>(name: string, value: NoInfer<T>, typeguard: (v: unknown) => v is T = (_: unknown): _ is any => true) => new StorageBound(window.sessionStorage, name, value, typeguard);
