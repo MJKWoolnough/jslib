6579f361608d365a045e51d8aebb07c75268c3f0
Michael Woolnough <michael.woolnough@gmail.com>
2024-04-18 21:37:32 +0100

    special case case folds that end in char 953, allowing for ranges to be used

--- casefold.sh
+++ casefold.sh
@@ -7,17 +7,25 @@ declare firstTo="";
 declare lastFrom="";
 declare lastTo="";
 declare mode=0;
+declare lastHad953=false;
+declare has953=false;
+declare toWithout953="";
 
 setStart() {
 	firstFrom="$from";
 	firstTo="$to";
 	setLast "$from" "$to";
 	mode=1;
+
+	if $has953; then
+		mode=4;
+		lastTo="$toWithout953";
+	fi;
 }
 
 setLast() {
-	lastFrom="$from";
-	lastTo="$to";
+	lastFrom="$1";
+	lastTo="$2";
 }
 
 printPrevious() {
@@ -41,13 +49,27 @@ printPrevious() {
 		fi;
 
 		echo "altAdd($firstFrom, $lastFrom$diff);";;
+	4)
+		if [ "$firstFrom" = "$lastFrom" ]; then
+			echo -n "add($firstFrom, ";
+			echo -n "$firstTo" | sed -e 's/,/, /';
+			echo ");";
+		else
+			if [ "$diff" = "32" ]; then
+				diff="";
+			else
+				diff=", $diff";
+			fi;
+
+			echo "addRange953($firstFrom, $lastFrom$diff);";
+		fi;;
 	esac;
 }
 
 declare source="$(
 	cat <<HEREDOC
 /**
- * The casefold module provides a simple unicode case-folding function.
+ * The casefold module provides a simple Unicode case-folding function.
  *
  * @module casefold
  */
@@ -60,6 +82,11 @@ const cf = new Map<string, string>(),#const cf = new Map(),
 		add(i, i + shift);
 	}
       },
+      addRange953 = (start: number, end: number, shift = 32) => {#      addRange953 = (start, end, shift = 32) => {
+	for (let i = start; i <= end; i++) {
+		add(i, i + shift, 953);
+	}
+      },
       altAdd = (start: number, end: number, shift = 1) => {#      altAdd = (start, end, shift = 1) => {
 	for (let i = start; i <= end; i += 2) {
 		add(i, i + shift);
@@ -70,8 +97,11 @@ HEREDOC
 	while read line; do
 		from="$(( $(echo "$line" | cut -d';' -f1 | sed -e 's/^0*/0x/') ))";
 		to="$(echo -n "$line" | cut -d';' -f3 | sed -e 's/^ *0*/0x/' -e 's/ 0*/ 0x/g' | tr ' ' '\n' | while read char; do echo $(( $char ));done | tr '\n' ',' | sed -e 's/,$//')";
-		
-		if echo "$to" | grep "," &> /dev/null; then
+
+		has953="$(echo "$to" | grep ",953$" > /dev/null && echo "true" || echo "false")";
+		toWithout953="$(echo "$to" | grep -v ",953$" || echo "$to" | sed -e 's/,953$//')";
+
+		if echo "$toWithout953" | grep "," &> /dev/null; then
 			printPrevious;
 			echo "add($from, ${to//,/, });";
 			mode=0;
@@ -103,9 +133,18 @@ HEREDOC
 				else
 					printPrevious;
 					setStart "$from" "$to";
-				fi;
+				fi;;
+			4)
+				if [ "$(( $lastFrom + 1 ))" = "$from" -a "$(( $lastTo + 1 ))" = "$toWithout953" -a $lastHad953 = $has953 ]; then
+					setLast "$from" "$toWithout953";
+				else
+					printPrevious;
+					setStart "$from" "$to";
+				fi;;
 			esac;
 		fi;
+
+		lastHad953=$has953;
 	done < <(curl "https://www.unicode.org/Public/UCD/latest/ucd/CaseFolding.txt" 2> /dev/null | grep -v " [ST];" | grep -v "^#" | grep -v "^$");
 
 	printPrevious;
