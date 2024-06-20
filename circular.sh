6ac88684988241b61ae0957d714fa6f654705db6
Michael Woolnough <michael.woolnough@gmail.com>
2024-06-20 21:28:21 +0100

    updated formatting to match new style

--- circular.sh
+++ circular.sh
@@ -4,24 +4,32 @@ filesDone=""
 
 function depCheck {
 	declare file="$1";
+
 	if [ ! -z "$(echo "$filesDone" | grep "$file")" ]; then
 		return 1;
 	fi;
+
 	declare deps="$(echo -e "$2\n$file")";
 	declare dir="$(dirname "$file")";
+
 	while read import; do
 		declare realImport="$(realpath "$dir/$import")";
+
 		if [ ! -z "$(echo "$deps" | grep "$realImport")" ]; then
 			echo "CIRCULAR: $file <-> $realImport"
+
 			return 0;
 		else
 			if depCheck "$realImport" "$deps"; then
 				echo "$realImport";
+
 				return 0;
 			fi;
 		fi;
 	done < <(grep "^import " "$file" | grep -v "^import type" | cut -d"'" -f2);
+
 	filesDone="$(echo -e "$filesDone\n$file")";
+
 	return 1;
 }
 
