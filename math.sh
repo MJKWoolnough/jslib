1ec9e51bfbaed579b39ccc2e1b80d1af14c569e9
Michael Woolnough <michael.woolnough@gmail.com>
2024-06-20 21:28:28 +0100

    updated formatting to match new style

--- math.sh
+++ math.sh
@@ -7,6 +7,7 @@ js="lib.js/math.js";
 
 if [ -n "$1" ]; then
 	tags="$1";
+
 	if [ -n "$2" ]; then
 		case "$2" in
 		*".js")
@@ -24,19 +25,23 @@ if [ -n "$ts" ]; then
 		echo "import type {DOMBind} from './dom.js';";
 		echo "import {bindElement} from './dom.js';";
 		echo -en "\n/**\n * The math module exports function for the creation of {@link https://developer.mozilla.org/en-US/docs/Web/API/MathMLElement | MathMLElement)s.\n *\n * @module math\n * @requires module:dom\n */\n/** */\n\nexport const\n/** This constant contains the XMLNamespace of MathMLElements. */\nns = \"http://www.w3.org/1998/Math/MathML\",\n[";
+
 		first=true;
+
 		for tag in $tags; do
 			if $first; then
 				first=false;
 			else
 				echo -n ", ";
 			fi;
+
 			if [ "$tag" = "annotation-xml" ]; then
 				echo -n "annotationXML";
 			else
 				echo -n "$tag";
 			fi;
 		done;
+
 		echo -n "] = \"$tags\".split(\" \").map(e => bindElement(ns, e)) as DOMBind<MathMLElement>[];";
 	) > "$ts";
 fi;
@@ -45,19 +50,23 @@ if [ -n "$js" ]; then
 	(
 		echo -e "import {bindElement} from './dom.js';\n";
 		echo -en "\n/**\n * The math module exports function for the creation of {@link https://developer.mozilla.org/en-US/docs/Web/API/MathMLElement | MathMLElement)s.\n *\n * @module math\n * @requires module:dom\n */\n/** */\n\nexport const\n/** This constant contains the XMLNamespace of MathMLElements. */\nns = \"http://www.w3.org/1998/Math/MathML\",\n[";
+
 		first=true;
+
 		for tag in $tags; do
 			if $first; then
 				first=false;
 			else
 				echo -n ", ";
 			fi;
+
 			if [ "$tag" = "annotation-xml" ]; then
 				echo -n "annotationXML";
 			else
 				echo -n "$tag";
 			fi;
 		done;
+
 		echo "] = \"$tags\".split(\" \").map(e => bindElement(ns, e));";
 	) > "$js";
 fi;
