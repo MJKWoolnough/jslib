529d3d72d4ebc667a712cbc45d68190b29020400
Michael Woolnough <michael.woolnough@gmail.com>
2024-06-20 21:28:36 +0100

    updated formatting to match new style

--- requiredHTML.sh
+++ requiredHTML.sh
@@ -5,20 +5,28 @@ tags="";
 
 function scanFile {
 	declare file="$1";
+
 	if [ ! -z "$(echo "$filesDone" | grep "$file")" ]; then
 		return;
 	fi;
+
 	filesDone="$(echo -e "$filesDone\n$file")";
+
 	declare dir="$(dirname "$file")";
 	declare import;
+
 	while read import; do
 		declare realImport="$(realpath "$dir/$(echo "$import" | cut -d"'" -f2)")";
+
 		scanFile "$realImport";
+
 		if [ ! -z "$(echo "$realImport" | sed -ne '/lib[^\/]*\/html.js$/p')" ]; then
 			if [ ! -z "$(echo "$import" | grep "*")" ]; then
 				echo "!!";
+
 				exit 1;
 			fi;
+
 			tags="$tags,$(echo "$import" | cut -d'{' -f2 | cut -d'}' -f1 | sed -e 's/ as [^,}]*//g' | tr -d ' ')";
 		fi;
 	done < <(grep "^import " "$file" | grep -v "^import type");
