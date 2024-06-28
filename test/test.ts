b8691815ad57299dfe10ea623d2189424b610eda
Michael Woolnough <michael.woolnough@gmail.com>
2024-06-28 21:17:23 +0100

    added null literal test for javascript tokeniser

--- test/test.ts
+++ test/test.ts
@@ -11752,6 +11752,10 @@ type Tests = {
 				"templates": {
 					"source": "`` `abc` `ab${ (val.a / 2) + 1 }c${str}`",
 					"output": "<span class=\"nosubstitutiontemplate\">``</span><span class=\"whitespace\">&nbsp;</span><span class=\"nosubstitutiontemplate\">`abc`</span><span class=\"whitespace\">&nbsp;</span><span class=\"templatehead\">`ab${</span><span class=\"whitespace\">&nbsp;</span><span class=\"punctuator\">(</span><span class=\"identifier\">val</span><span class=\"punctuator\">.</span><span class=\"identifier\">a</span><span class=\"whitespace\">&nbsp;</span><span class=\"divpunctuator\">/</span><span class=\"whitespace\">&nbsp;</span><span class=\"numericliteral\">2</span><span class=\"punctuator\">)</span><span class=\"whitespace\">&nbsp;</span><span class=\"punctuator\">+</span><span class=\"whitespace\">&nbsp;</span><span class=\"numericliteral\">1</span><span class=\"whitespace\">&nbsp;</span><span class=\"templatemiddle\">}c${</span><span class=\"identifier\">str</span><span class=\"templatetail\">}`</span>"
+				},
+				"null": {
+					"source": "null",
+					"output": `<span class="nullliteral">null</span>`
 				}
 			}
 		} as Record<string, Record<string, {source: string; output: string}>>).reduce((o, [testname, tests]) => (o[testname] = Object.entries(tests).reduce((o, [name, {source, output}]) => (o[name] = async () => {
