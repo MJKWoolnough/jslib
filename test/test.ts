0b72108cac43dd08c4978b896a842a6f2cc5822c
Michael Woolnough <michael.woolnough@gmail.com>
2024-07-04 21:43:22 +0100

    add python test for parsing operators/deliminators

--- test/test.ts
+++ test/test.ts
@@ -11822,6 +11822,10 @@ type Tests = {
 				"float": {
 					"source": "0.123 .456 3.14159E3j",
 					"output": `<span class="numericliteral">0.123</span><span class="whitespace">&nbsp;</span><span class="numericliteral">.456</span><span class="whitespace">&nbsp;</span><span class="numericliteral">3.14159E3j</span>`
+				},
+				"punctuators": {
+					"source": "+ - @= <<= () !=",
+					"output": `<span class="punctuator">+</span><span class="whitespace">&nbsp;</span><span class="punctuator">-</span><span class="whitespace">&nbsp;</span><span class="punctuator">@=</span><span class="whitespace">&nbsp;</span><span class="punctuator">&lt;&lt;=</span><span class="whitespace">&nbsp;</span><span class="punctuator">()</span><span class="whitespace">&nbsp;</span><span class="punctuator">!=</span>`
 				}
 			}
 		} as Record<string, Record<string, {source: string; output: string}>>).reduce((o, [testname, tests]) => (o[testname] = Object.entries(tests).reduce((o, [name, {source, output}]) => (o[name] = async () => {
