625327e81ca8a153dc2ecdbda0bc3b8a258247f9
Michael Woolnough <michael.woolnough@gmail.com>
2024-07-07 18:37:51 +0100

    initial child parse

--- lib.ts/multiselect.ts
+++ lib.ts/multiselect.ts
@@ -1,13 +1,24 @@
-class MultiSelect extends HTMLElement {
+import {amendNode, clearNode} from './dom.js';
+import {li, ul} from './html.js';
+
+export class MultiSelect extends HTMLElement {
+	#options: HTMLUListElement;
+	#liToOption = new Map<HTMLLIElement, HTMLOptionElement>();
+	#optionToLI = new Map<HTMLOptionElement, HTMLLIElement>();
+
 	constructor() {
 		super();
+
+		amendNode(this.attachShadow({"mode": "closed", "slotAssignment": "manual"}), [
+			this.#options = ul()
+		]);
 
 		this.#parseContent();
 
 		const mo = new MutationObserver(mutations => this.#handleMutations(mutations));
 
 		mo.observe(this, {
-			"attributeFilter": ["value", "disabled"],
+			"attributeFilter": ["value", "disabled", "label"],
 			"childList": true,
 			"subtree": true
 		});
@@ -16,5 +27,28 @@ class MultiSelect extends HTMLElement {
 	#handleMutations(_mutations: MutationRecord[]) {
 	}
 
-	#parseContent() {};
+	#parseContent() {
+		const newElems: HTMLLIElement[] = [],
+		      disabled = {"class": {"disabled": false}},
+		      oldOptions = this.#optionToLI;
+
+		this.#optionToLI = new Map();
+		this.#liToOption.clear();
+
+		for (const elem of this.children) {
+			if (elem instanceof HTMLOptionElement) {
+				disabled.class.disabled = elem.hasAttribute("disabled");
+
+				const text = elem.getAttribute("label") ?? elem.innerText,
+				      existing = oldOptions.get(elem),
+				      item = existing?.innerText === text ? amendNode(existing, disabled) : li(disabled, text);
+
+				newElems.push(item);
+				this.#optionToLI.set(elem, item);
+				this.#liToOption.set(item, elem);
+			}
+		}
+
+		clearNode(this.#options, newElems);
+	};
 }
