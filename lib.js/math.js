4b6d629510428ed1df666648b172a0cdb13c703c
Michael Woolnough <michael.woolnough@gmail.com>
2024-08-02 20:19:21 +0100

    re-gen'd math lib

--- lib.js/math.js
+++ lib.js/math.js
@@ -1,4 +1,4 @@
-import {bindElement} from './dom.js';
+import {tags} from './dom.js';
 
 
 /**
@@ -12,4 +12,4 @@ import {bindElement} from './dom.js';
 export const
 /** This constant contains the XMLNamespace of MathMLElements. */
 ns = "http://www.w3.org/1998/Math/MathML",
-[annotation, annotationXML, maction, math, menclose, merror, mfenced, mfrac, mi, mmultiscripts, mn, mo, mover, mpadded, mphantom, mprescripts, mroot, mrow, ms, mspace, msqrt, mstyle, msub, msubsup, msup, mtable, mtd, mtext, mtr, munder, munderover, semantics] = "annotation annotation-xml maction math menclose merror mfenced mfrac mi mmultiscripts mn mo mover mpadded mphantom mprescripts mroot mrow ms mspace msqrt mstyle msub msubsup msup mtable mtd mtext mtr munder munderover semantics".split(" ").map(e => bindElement(ns, e));
+[annotation, annotationXML, maction, math, menclose, merror, mfenced, mfrac, mi, mmultiscripts, mn, mo, mover, mpadded, mphantom, mprescripts, mroot, mrow, ms, mspace, msqrt, mstyle, msub, msubsup, msup, mtable, mtd, mtext, mtr, munder, munderover, semantics] = tags(ns);
--- lib.ts/math.ts
+++ lib.ts/math.ts
@@ -1,5 +1,4 @@
-import type {DOMBind} from './dom.js';
-import {bindElement} from './dom.js';
+import {tags} from './dom.js';
 
 /**
  * The math module exports function for the creation of {@link https://developer.mozilla.org/en-US/docs/Web/API/MathMLElement | MathMLElement)s.
@@ -12,4 +11,4 @@ import {bindElement} from './dom.js';
 export const
 /** This constant contains the XMLNamespace of MathMLElements. */
 ns = "http://www.w3.org/1998/Math/MathML",
-[annotation, annotationXML, maction, math, menclose, merror, mfenced, mfrac, mi, mmultiscripts, mn, mo, mover, mpadded, mphantom, mprescripts, mroot, mrow, ms, mspace, msqrt, mstyle, msub, msubsup, msup, mtable, mtd, mtext, mtr, munder, munderover, semantics] = "annotation annotation-xml maction math menclose merror mfenced mfrac mi mmultiscripts mn mo mover mpadded mphantom mprescripts mroot mrow ms mspace msqrt mstyle msub msubsup msup mtable mtd mtext mtr munder munderover semantics".split(" ").map(e => bindElement(ns, e)) as DOMBind<MathMLElement>[];
+{annotation, annotationXML, maction, math, menclose, merror, mfenced, mfrac, mi, mmultiscripts, mn, mo, mover, mpadded, mphantom, mprescripts, mroot, mrow, ms, mspace, msqrt, mstyle, msub, msubsup, msup, mtable, mtd, mtext, mtr, munder, munderover, semantics} = tags(ns);
