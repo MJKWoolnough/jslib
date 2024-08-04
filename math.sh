44b993eebc3b4bfcb14655d66a310070baf27af5
Michael Woolnough <michael.woolnough@gmail.com>
2024-08-04 18:35:28 +0100

    simplified math module generator

--- math.sh
+++ math.sh
@@ -3,33 +3,25 @@
 tags="annotation annotation-xml maction math menclose merror mfenced mfrac mi mmultiscripts mn mo mover mpadded mphantom mprescripts mroot mrow ms mspace msqrt mstyle msub msubsup msup mtable mtd mtext mtr munder munderover semantics";
 
 (
-	echo "import {tags} from './dom.js';";
-	echo -en "\n/**\n * The math module exports function for the creation of {@link https://developer.mozilla.org/en-US/docs/Web/API/MathMLElement | MathMLElement)s.\n *\n * @module math\n * @requires module:dom\n */\n/** */\n\nexport const\n/** This constant contains the XMLNamespace of MathMLElements. */\nns = \"http://www.w3.org/1998/Math/MathML\",\n{";
+	cat <<-HEREDOC
+	import {tags} from './dom.js';
 
-	first=true;
+	/**
+	 * The math module exports function for the creation of {@link https://developer.mozilla.org/en-US/docs/Web/API/MathMLElement | MathMLElement)s.
+	 *
+	 * @module math
+	 * @requires module:dom
+	 */
+	/** */
 
-	for tag in $tags; do
-		if $first; then
-			first=false;
-		else
-			echo -n ", ";
-		fi;
+	export const
+	/** This constant contains the XMLNamespace of MathMLElements. */
+	ns = "http://www.w3.org/1998/Math/MathML",
+	HEREDOC
 
-		if [ "$tag" = "annotation-xml" ]; then
-			echo -n "annotationXML";
-		else
-			echo -n "$tag";
-		fi;
-	done;
+	echo -n "{";
 
-	echo -n "} = tags(ns);";
-) > lib.ts/math.ts;
-
-(
-	echo -e "import {tags} from './dom.js';\n";
-	echo -en "\n/**\n * The math module exports function for the creation of {@link https://developer.mozilla.org/en-US/docs/Web/API/MathMLElement | MathMLElement)s.\n *\n * @module math\n * @requires module:dom\n */\n/** */\n\nexport const\n/** This constant contains the XMLNamespace of MathMLElements. */\nns = \"http://www.w3.org/1998/Math/MathML\",\n[";
-
-	first=true;
+	declare first=true;
 
 	for tag in $tags; do
 		if $first; then
@@ -45,5 +37,5 @@ tags="annotation annotation-xml maction math menclose merror mfenced mfrac mi mm
 		fi;
 	done;
 
-	echo "] = tags(ns);";
-) > lib.js/math.js;
+	echo "} = tags(ns);";
+) | tee lib.ts/math.ts > lib.js/math.js;
