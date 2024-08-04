15bd25d30d2824ecba358ad43079302bbb897931
Michael Woolnough <michael.woolnough@gmail.com>
2024-08-04 18:35:47 +0100

    regen'd

--- lib.js/math.js
+++ lib.js/math.js
@@ -1,6 +1,5 @@
 import {tags} from './dom.js';
 
-
 /**
  * The math module exports function for the creation of {@link https://developer.mozilla.org/en-US/docs/Web/API/MathMLElement | MathMLElement)s.
  *
@@ -12,4 +11,4 @@ import {tags} from './dom.js';
 export const
 /** This constant contains the XMLNamespace of MathMLElements. */
 ns = "http://www.w3.org/1998/Math/MathML",
-[annotation, annotationXML, maction, math, menclose, merror, mfenced, mfrac, mi, mmultiscripts, mn, mo, mover, mpadded, mphantom, mprescripts, mroot, mrow, ms, mspace, msqrt, mstyle, msub, msubsup, msup, mtable, mtd, mtext, mtr, munder, munderover, semantics] = tags(ns);
+{annotation, annotationXML, maction, math, menclose, merror, mfenced, mfrac, mi, mmultiscripts, mn, mo, mover, mpadded, mphantom, mprescripts, mroot, mrow, ms, mspace, msqrt, mstyle, msub, msubsup, msup, mtable, mtd, mtext, mtr, munder, munderover, semantics} = tags(ns);
