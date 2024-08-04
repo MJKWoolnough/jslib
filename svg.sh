f344b765583293a4119e03a88c821e0bd3928294
Michael Woolnough <michael.woolnough@gmail.com>
2024-08-04 18:36:58 +0100

    simplified svg module generator

--- svg.sh
+++ svg.sh
@@ -4,33 +4,32 @@
 tags="a animate animateMotion animateTransform circle clipPath defs desc ellipse feBlend feColorMatrix feComponentTransfer feComposite feConvolveMatrix feDiffuseLighting feDisplacementMap feDistantLight feDropShadow feFlood feFuncA feFuncB feFuncG feFuncR feGaussianBlur feImage feMerge feMergeNode feMorphology feOffset fePointLight feSpecularLighting feSpotLight feTile feTurbulence filter foreignObject g image line linearGradient marker mask metadata mpath path pattern polygon polyline radialGradient rect script set stop style svg switch symbol text textPath title tspan use view";
 
 (
-	echo "import {tags} from './dom.js';";
-	echo -en "\n/**\n * The svg module exports function for the creation of {@link https://developer.mozilla.org/en-US/docs/Web/API/SVGElement | SVGElement)s.\n *\n * @module svg\n * @requires module:dom\n */\n/** */\n\nexport const\n/** This constant contains the XMLNamespace of SVGElements. */\nns = \"http://www.w3.org/2000/svg\",\n/**\n * This function takes either a {@link https://developer.mozilla.org/en-US/docs/Web/API/SVGSVGElement | SVGSVGElement} or a {@link https://developer.mozilla.org/en-US/docs/Web/API/SVGSymbolElement | SVGSymbolElement} and returns a URL encoded SVG data string.\n * @param {SVGSVGElement | SVGSymbolElement} s The SVG or Symbol element to be stringified.\n *\n * @return {string} The string representation of the input.\n */\nsvgData = (s: SVGSVGElement | SVGSymbolElement) => \"data:image/svg+xml,\" + encodeURIComponent(\"<svg xmlns=\\\"\" + ns + \"\\\"\" + (s instanceof SVGSVGElement ? s.outerHTML.slice(4) : s.outerHTML.slice(7, -7) + \"svg>\")),\n{";
+	cat <<-HEREDOC
+	import {tags} from './dom.js';
 
-	first=true;
+	/**
+	 * The svg module exports function for the creation of {@link https://developer.mozilla.org/en-US/docs/Web/API/SVGElement | SVGElement)s.
+	 *
+	 * @module svg
+	 * @requires module:dom
+	 */
+	/** */
 
-	for tag in $tags; do
-		if $first; then
-			first=false;
-		else
-			echo -n ", ";
-		fi;
+	export const
+	/** This constant contains the XMLNamespace of SVGElements. */
+	ns = "http://www.w3.org/2000/svg",
+	/**
+	 * This function takes either a {@link https://developer.mozilla.org/en-US/docs/Web/API/SVGSVGElement | SVGSVGElement} or a {@link https://developer.mozilla.org/en-US/docs/Web/API/SVGSymbolElement | SVGSymbolElement} and returns a URL encoded SVG data string.
+	 * @param {SVGSVGElement | SVGSymbolElement} s The SVG or Symbol element to be stringified.
+	 *
+	 * @return {string} The string representation of the input.
+	 */
+	svgData = (s: SVGSVGElement | SVGSymbolElement) => "data:image/svg+xml," + encodeURIComponent("<svg xmlns=\"" + ns + "\"" + (s instanceof SVGSVGElement ? s.outerHTML.slice(4) : s.outerHTML.slice(7, -7) + "svg>")),
+	HEREDOC
 
-		if [ "$tag" = "switch" ]; then
-			echo -n "switch: switche";
-		else
-			echo -n "$tag";
-		fi;
-	done;
+	echo -n "{";
 
-	echo -n "} = tags(ns);";
-) > lib.ts/svg.ts;
-
-(
-	echo "import {tags} from './dom.js';";
-	echo -en "\n/**\n * The svg module exports function for the creation of {@link https://developer.mozilla.org/en-US/docs/Web/API/SVGElement | SVGElement)s.\n *\n * @module svg\n * @requires module:dom\n */\n/** */\n\nexport const\n/** This constant contains the XMLNamespace of SVGElements. */\nns = \"http://www.w3.org/2000/svg\",\n/**\n * This function takes either a {@link https://developer.mozilla.org/en-US/docs/Web/API/SVGSVGElement | SVGSVGElement} or a {@link https://developer.mozilla.org/en-US/docs/Web/API/SVGSymbolElement | SVGSymbolElement} and returns a URL encoded SVG data string.\n * @param {SVGSVGElement | SVGSymbolElement} s The SVG or Symbol element to be stringified.\n *\n * @return {string} The string representation of the input.\n */\nsvgData = s => \"data:image/svg+xml,\" + encodeURIComponent(\"<svg xmlns=\\\"\" + ns + \"\\\"\" + (s instanceof SVGSVGElement ? s.outerHTML.slice(4) : s.outerHTML.slice(7, -7) + \"svg>\")),\n{";
-
-	first=true;
+	declare first=true;
 
 	for tag in $tags; do
 		if $first; then
@@ -47,4 +46,4 @@ tags="a animate animateMotion animateTransform circle clipPath defs desc ellipse
 	done;
 
 	echo "} = tags(ns);";
-) > lib.js/svg.js;
+) | tee lib.ts/svg.ts | sed -e 's/(s: [^)]*)/s/' > lib.js/svg.js;
