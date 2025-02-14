1400ec74ada4c5e677f753bcee415ea91bb7b0c6
Michael Woolnough <michael.woolnough@gmail.com>
2025-02-14 08:36:00 +0000

    added test for R integers

--- test/test.ts
+++ test/test.ts
@@ -13577,6 +13577,10 @@ type Tests = {
 				"identifiers": {
 					"source": "an identifier\na_nother1",
 					"output": `<span class="identifier">an</span><span class="whitespace">&nbsp;</span><span class="identifier">identifier</span><span class="lineterminator"><br></span><span class="identifier">a_nother1</span>`
+				},
+				"integers": {
+					"source": "123 456 1e10 1i 123e-456",
+					"output": `<span class="numericliteral">123</span><span class="whitespace">&nbsp;</span><span class="numericliteral">456</span><span class="whitespace">&nbsp;</span><span class="numericliteral">1e10</span><span class="whitespace">&nbsp;</span><span class="numericliteral">1i</span><span class="whitespace">&nbsp;</span><span class="numericliteral">123e-456</span>`
 				}
 			}
 		} as Record<string, Record<string, {source: string; output: string}>>).reduce((o, [testname, tests]) => (o[testname] = Object.entries(tests).reduce((o, [name, {source, output}]) => (o[name] = async () => {
